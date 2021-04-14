458e48de7b5cc761b72d217371ed2be88cae8288
jurjen93 <jurjendejong93@gmail.com>
2021-04-14 16:40:57 +0200

    update extract.sh

--- pipeline_scripts/moveFiles_extract.sh
+++ pipeline_scripts/moveFiles_extract.sh
@@ -12,7 +12,7 @@ TO=$2 #lofarvwf-jdejong@spider.surfsara.nl:/project/lofarvwf/Share/jdejong/data/
 
 cd /net/tussenrijn/data2/jurjendejong/L626678
 
-tar -cvzf data_archive.tar.gz --absolute-names ${FROM}/image_full_ampphase_di_m.NS.tessel.reg ${FROM}/image_full_ampphase_di_m.NS.mask01.fits ${FROM}/image_full_ampphase_di_m.NS.DicoModel ${FROM}/image_dirin_SSD_m.npy.ClusterCat.npy ${FROM}/DDS3_full_*.01_merged.npz ${FROM}/DDS3_full_*.01_smoothed.npz ${FROM}/*_uv.pre-cal_*.pre-cal.ms.archive ${FROM}/SOLSDIR ${TO}
+tar -C ${FROM} -cvzf data_archive.tar.gz --absolute-names image_full_ampphase_di_m.NS.tessel.reg image_full_ampphase_di_m.NS.mask01.fits image_full_ampphase_di_m.NS.DicoModel image_dirin_SSD_m.npy.ClusterCat.npy DDS3_full_*.01_merged.npz DDS3_full_*.01_smoothed.npz *_uv.pre-cal_*.pre-cal.ms.archive SOLSDIR
 
 scp -r /net/tussenrijn/data2/jurjendejong/L626678/data_archive.tar.gz ${TO}
 
