e40e2322baefe1a563d04363c6eb79a6a25dc79a
Jurjen Jong <jurjendejong@vdesk5.strw.leidenuniv.nl>
2021-04-14 10:26:35 +0200

    updates

--- merge_selfcals.py
+++ merge_selfcals.py
@@ -6,6 +6,12 @@ parser = ArgumentParser()
 parser.add_argument('-d', '--directory', type=str, help='directory path')
 args = parser.parse_args()
 
+h5_files = []
+for box in glob('{directory}/box_*'.format(directory=args.directory)):
+  try:
+    h5_files.append(sorted(glob('{box}/merged_selfcalcyle*_*.ms.*h5'.format(box=box)))[-1])
+  except:
+    print("No merged_selfcal* in {box}".format(box=box.split('/')[-1]))
+
 merge_h5(h5_out='all_directions.h5',
-         h5_files=[sorted(glob('{box}/merged_selfcalcyle*_*.ms.h5'.format(box=box)))[-1] 
-	   for box in glob('{directory}/box_*'.format(directory=args.directory))])
+         h5_files=h5_files)
