8574bfde6885dd0ac6e1397f2a9cc177ae8fdf76
jurjen93 <jurjendejong93@gmail.com>
2021-04-19 19:55:27 +0200

    N added

--- pipeline_scripts/selfcal_per_box.sh
+++ pipeline_scripts/selfcal_per_box.sh
@@ -7,7 +7,7 @@ echo "----------START----------"
 SOURCE=$1 #L626678
 TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}
 SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts
-i=$2#box number
+N=$2 #box number
 
 SING_IMAGE=/home/lofarvwf-jdejong/singularities/pill-latest.simg
 SING_BIND=/project/lofarvwf/Share/jdejong
@@ -15,14 +15,15 @@ SING_BIND=/project/lofarvwf/Share/jdejong
 #SELFCAL
 
 echo "-----STARTED SELFCAL-----"
+echo "Started selfcal for box_${N}"
 cd ${TO}/selfcal/
-mkdir ${TO}/selfcal/box_${i}
-cp -r ${TO}/extract/*box_${i}.dysco.sub.shift.avg.weights.ms.archive0 ${TO}/selfcal/
-singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=${TO}/selfcal/Abell399-401_box_${i}.dysco.sub.shift.avg.weights.ms.archive0 msout.storagemanager=dysco msout=${TO}/selfcal/box_${i}/box_${i}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes msin.ntimes=1500 steps=[]
-cd ${TO}/selfcal/box_${i}
-singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/runwscleanLBautoR.py -b ${TO}/boxes/box_${i}.reg --auto box_${i}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes
-# singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/runwscleanLBauto.py -b ${TO}/boxes/box_${i}.reg --forwidefield --smoothnessconstraint-slow=5.0 --usemodeldataforsolints --slow-soltype=complexgain --usewgridder --avgfreqstep=2 --avgtimestep=2 --imager=DDFACET --useaoflagger --noarchive box_${i}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes
-echo "Finished selfcal for box_${i}"
+mkdir ${TO}/selfcal/box_${N}
+cp -r ${TO}/extract/*box_${N}.dysco.sub.shift.avg.weights.ms.archive0 ${TO}/selfcal/
+singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=${TO}/selfcal/Abell399-401_box_${N}.dysco.sub.shift.avg.weights.ms.archive0 msout.storagemanager=dysco msout=${TO}/selfcal/box_${N}/box_${N}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes msin.ntimes=1500 steps=[]
+cd ${TO}/selfcal/box_${N}
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/runwscleanLBautoR.py -b ${TO}/boxes/box_${N}.reg --auto box_${N}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes
+# singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/runwscleanLBauto.py -b ${TO}/boxes/box_${N}.reg --forwidefield --smoothnessconstraint-slow=5.0 --usemodeldataforsolints --slow-soltype=complexgain --usewgridder --avgfreqstep=2 --avgtimestep=2 --imager=DDFACET --useaoflagger --noarchive box_${N}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes
+echo "Finished selfcal for box_${N}"
 echo "-----FINISHED SELFCAL-----"
 
 #merge selfcals
