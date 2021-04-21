188b83d8cda228ee0751539be97e6acf50ddbcac
jurjen93 <jurjendejong93@gmail.com>
2021-04-21 13:33:15 +0200

    more cores

--- pipeline_scripts/parallel_selfcal.sh
+++ pipeline_scripts/parallel_selfcal.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SBATCH -c 90
 
 SOURCE=$1 #L626678
 TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}
@@ -9,7 +10,7 @@ START_N=1
 END_N=$(ls -dq ${TO}/extract/*.dysco.sub.shift.avg.weights.ms.archive* | wc -l)
 
 #PARAMETER SWEEP
-for i in 2 3 4; do
+for ((i=${START_N};i<=${END_N};i++)); do
   sh ${SCRIPT_FOLDER}/selfcal_per_box.sh ${SOURCE} ${i} &
 done
 wait
