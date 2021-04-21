1f2efc246bf1a31db6b8548ed27224b9021510c1
jurjen93 <jurjendejong93@gmail.com>
2021-04-21 14:30:43 +0200

    srun update

--- pipeline_scripts/parallel_selfcal.sh
+++ pipeline_scripts/parallel_selfcal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH -c 90
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
 
 SOURCE=$1 #L626678
 TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}
@@ -9,8 +10,11 @@ SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers/pipeline_scripts
 START_N=1
 END_N=$(ls -dq ${TO}/extract/*.dysco.sub.shift.avg.weights.ms.archive* | wc -l)
 
+START_N=2
+END_N=5
+
 #PARAMETER SWEEP
 for ((i=${START_N};i<=${END_N};i++)); do
-  sh ${SCRIPT_FOLDER}/selfcal_per_box.sh ${SOURCE} ${i} &
+  srun -c 30 sh ${SCRIPT_FOLDER}/selfcal_per_box.sh ${SOURCE} ${i} &
 done
 wait
