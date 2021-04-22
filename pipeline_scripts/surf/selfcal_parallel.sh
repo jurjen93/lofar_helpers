a451922efa429013688e7b3072cc50d8e71b88ca
Jurjen de Jong <lofarvwf-jdejong@ui-01.spider.surfsara.nl>
2021-04-22 15:30:58 +0200

    pipeline batch upgrade

--- pipeline_scripts/surf/selfcal_parallel.sh
+++ pipeline_scripts/surf/selfcal_parallel.sh
@@ -5,18 +5,19 @@
 SOURCE=$1 #L626678
 TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}
 
-SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers/pipeline_scripts
+SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers/pipeline_scripts/surf
 
-START_N=1
-END_N=$(ls -dq ${TO}/extract/*.dysco.sub.shift.avg.weights.ms.archive* | wc -l)
+#START_N=1
+#END_N=$(ls -dq ${TO}/extract/L626678/*.dysco.sub.shift.avg.weights.ms.archive* | wc -l)
 
 START_N=2
-END_N=5
+END_N=4
 
 #TODO: FIX PARALLEL. How to do best?
 
 #PARAMETER SWEEP
 for ((i=${START_N};i<=${END_N};i++)); do
-  srun sh ${SCRIPT_FOLDER}/selfcal_per_box.sh ${SOURCE} ${i} &
+    sbatch ${SCRIPT_FOLDER}/selfcal_per_box.sh ${SOURCE} ${i}
+    sleep 1
 done
 wait
