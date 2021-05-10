e6366aa1985f05c9d8b4c778b14dedc3d221de3a
jurjen93 <jurjendejong93@gmail.com>
2021-05-10 10:25:36 +0200

    sort

--- pipeline_scripts/surf/show_results.py
+++ pipeline_scripts/surf/show_results.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     boxes = glob('{path}/box_*'.format(path=path))
 
     for box in boxes:
-        images = glob('{box_path}/image_*.png'.format(box_path=box))
-        images.sort()
+        images = sorted(glob('{box_path}/image_*.png'.format(box_path=box)))
+        print(images)
         first_image, last_image = images[0], images[-1]
         fig, axs = plt.subplots(1, 2, figsize=(18,6))
         im_0 = mpimg.imread(first_image)
