c309aabab58212a3aece2ab384dd29e671cf2c5a
jurjen93 <jurjendejong93@gmail.com>
2021-05-21 16:37:02 +0200

    cacheclean

--- pipeline_scripts/strw/make_new_image.py
+++ pipeline_scripts/strw/make_new_image.py
@@ -14,6 +14,8 @@ SING_IMAGE='/net/rijn/data2/rvweeren/data/pill-latestMay2021.simg'
 SING_BIND='/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2'
 SINGULARITY=' '.join(['singularity exec -B', SING_BIND, SING_IMAGE])
 
+os.system('{SINGULARITY} CleanSHM.py'.format(SINGULARITY=SINGULARITY))
+
 #MAKE RESULT FOLDER
 # os.system('mkdir '+LOCATION)
 # os.system('mkdir '+LOCATION_BACKUP)
@@ -30,10 +32,10 @@ print('Finished moving files')
 #CUT TIME FOR MESSY END PART (ONLY FOR THIS CASE APPLICABLE)
 print('Making goodtimes')
 os.system('cd '+LOCATION)
-for MS in glob('{LOCATION}/*_uv.pre-cal_*.pre-cal.ms.archive'.format(LOCATION=LOCATION)):
-    os.system(SINGULARITY+' DPPP msin='+MS+' msout.storagemanager=dysco msout='+MS+'.goodtimes msin.ntimes=1500 steps=[]')
-    os.system('mv '+MS+' '+LOCATION_BACKUP)
-    print('Made '+MS+'.goodtimes')
+# for MS in glob('{LOCATION}/*_uv.pre-cal_*.pre-cal.ms.archive'.format(LOCATION=LOCATION)):
+#     os.system(SINGULARITY+' DPPP msin='+MS+' msout.storagemanager=dysco msout='+MS+'.goodtimes msin.ntimes=1500 steps=[]')
+#     os.system('mv '+MS+' '+LOCATION_BACKUP)
+#     print('Made '+MS+'.goodtimes')
 
 #MAKE LIST WITH MEASUREMENT SETS
 # os.system('ls -1d {LOCATION}/*.goodtimes > {LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION))
