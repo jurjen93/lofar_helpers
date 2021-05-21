cc46b21f798ce82191a26e133fd4ea8f9d5de1b1
jurjen93 <jurjendejong93@gmail.com>
2021-05-21 16:08:01 +0200

    ddf command update

--- pipeline_scripts/strw/make_new_image.py
+++ pipeline_scripts/strw/make_new_image.py
@@ -13,12 +13,11 @@ FROM=args.from_where #/disks/paradata/shimwell/LoTSS-DR2/archive_other/L626678
 SING_IMAGE='/net/rijn/data2/rvweeren/data/pill-latestMay2021.simg'
 SING_BIND='/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2'
 SINGULARITY=' '.join(['singularity exec -B', SING_BIND, SING_IMAGE])
-RUN = [SINGULARITY, 'python']
 
 #MAKE RESULT FOLDER
-os.system('mkdir '+LOCATION)
-os.system('mkdir '+LOCATION_BACKUP)
-print('Made '+LOCATION)
+# os.system('mkdir '+LOCATION)
+# os.system('mkdir '+LOCATION_BACKUP)
+# print('Made '+LOCATION)
 
 #MOVE FILES
 print('Moving files to '+LOCATION)
@@ -30,23 +29,19 @@ print('Finished moving files')
 
 #CUT TIME FOR MESSY END PART (ONLY FOR THIS CASE APPLICABLE)
 print('Making goodtimes')
-os.system('cd '+LOCATION)
-for MS in glob('{LOCATION}/*_uv.pre-cal_*.pre-cal.ms.archive'.format(LOCATION=LOCATION)):
-    os.system(SINGULARITY+' DPPP msin='+MS+' msout.storagemanager=dysco msout='+MS+'.goodtimes msin.ntimes=1500 steps=[]')
-    os.system('mv '+MS+' '+LOCATION_BACKUP)
-    print('Made '+MS+'.goodtimes')
+# os.system('cd '+LOCATION)
+# for MS in glob('{LOCATION}/*_uv.pre-cal_*.pre-cal.ms.archive'.format(LOCATION=LOCATION)):
+#     os.system(SINGULARITY+' DPPP msin='+MS+' msout.storagemanager=dysco msout='+MS+'.goodtimes msin.ntimes=1500 steps=[]')
+#     os.system('mv '+MS+' '+LOCATION_BACKUP)
+#     print('Made '+MS+'.goodtimes')
 
 #MAKE LIST WITH MEASUREMENT SETS
-os.system('ls -1d {LOCATION}/*.goodtimes > {LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION))
+# os.system('ls -1d {LOCATION}/*.goodtimes > {LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION))
 
 with open('/home/jurjendejong/scripts/lofar_helpers/pipeline_scripts/strw/ddf.txt') as f:
     lines = [l.replace('\n','') for l in f.readlines()]
 
-#MAKE DDF COMMAND READY TO RUN
-RUN += lines
-DDF_COMMAND = ' '.join(RUN)
-
 #RUN DDF COMMAND
 print('Running DDF COMMAND')
-os.system(DDF_COMMAND)
+os.system(' '.join([SINGULARITY] + lines))
 print('Finished making new image')
