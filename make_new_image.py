e947c931cd3b98cfb0d2def5e5ee9a855711293b
jurjen93 <jurjendejong93@gmail.com>
2021-05-27 09:45:57 +0200

    update

--- make_new_image.py
+++ make_new_image.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 
 LOCATION=args.to_where#/net/tussenrijn/data2/jurjendejong/L626678
 FROM=args.from_where #/disks/paradata/shimwell/LoTSS-DR2/archive_other/L626678
-SING_IMAGE='/net/rijn/data2/rvweeren/data/pill-latestMay2021.simg'
+SING_IMAGE='/net/rijn/data2/rvweeren/data/pill-latest.simg'
 SING_BIND='/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2'
 SINGULARITY=' '.join(['singularity exec -B', SING_BIND, SING_IMAGE])
 
@@ -36,7 +36,7 @@ print('Finished moving files')
 if args.time_flag:
     print('Making goodtimes')
     for MS in glob('{LOCATION}/*_uv.pre-cal_*.pre-cal.ms.archive'.format(LOCATION=LOCATION)):
-        os.system('{SINGULARITY} python supporting_scripts/flag_time.py -tf {time[0]} {time[1]} -ms {MS}'.format(SINGULARITY=SINGULARITY, MS=MS, time=args.flag_time))
+        os.system('{SINGULARITY} python supporting_scripts/flag_time.py -tf {time} -ms {MS}'.format(SINGULARITY=SINGULARITY, MS=MS, time=' '.join(args.flag_time)))
 
 #MAKE LIST WITH MEASUREMENT SETS
 os.system('ls -1d {LOCATION}/*.goodtimes > {LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION))
