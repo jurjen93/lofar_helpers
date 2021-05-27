a69b0600c3530d25364b8243f831f853a10f5454
jurjen93 <jurjendejong93@gmail.com>
2021-05-27 10:41:04 +0200

    no test

--- h5_merger.py
+++ h5_merger.py
@@ -317,7 +317,6 @@ class MergeH5:
                 print('Adding new direction {:f},{:f}'.format(*source_coords))
                 idx = self.n
                 self.directions.update({d: source_coords})
-                print(self.directions)
                 self.n += 1
                 if self.n>1:# for n==0 we dont have to do anything
                     if st.getType() in ['tec','phase','rotation']:
