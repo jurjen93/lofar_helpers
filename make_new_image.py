5ae17635a90e04749063f35b23c5e14387f74f1b
jurjen93 <jurjendejong93@gmail.com>
2021-06-10 15:50:09 +0200

    u

--- make_new_image.py
+++ make_new_image.py
@@ -18,9 +18,9 @@ SINGULARITY=' '.join(['singularity exec -B', SING_BIND, SING_IMAGE])
 # SINGULARITY=' '.join(['singularity exec ', SING_IMAGE])
 
 #CREATE DESTINATION DIRECTORY IF NOT EXISTS
-if not path.exists(LOCATION):
-    os.system('mkdir {LOCATION}'.format(LOCATION=LOCATION))
-    print('Created {LOCATION}'.format(LOCATION=LOCATION))
+# if not path.exists(LOCATION):
+#     os.system('mkdir {LOCATION}'.format(LOCATION=LOCATION))
+#     print('Created {LOCATION}'.format(LOCATION=LOCATION))
 
 #CLEAN CACHE
 os.system('{SINGULARITY} CleanSHM.py'.format(SINGULARITY=SINGULARITY))
@@ -28,9 +28,9 @@ os.system('{SINGULARITY} CleanSHM.py'.format(SINGULARITY=SINGULARITY))
 #MOVE FILES
 print('Moving files to '+LOCATION)
 # os.system("scp -r lofarvwf-jdejong@spider.surfsara.nl:/project/lofarvwf/Share/jdejong/output/L626678/selfcal/all_directions.h5 "+LOCATION)
-os.system('cp -r '+FROM+'/image_full_ampphase_di_m.NS.mask01.fits '+LOCATION)
-os.system('cp -r '+FROM+'/image_full_ampphase_di_m.NS.DicoModel '+LOCATION)
-os.system('cp -r '+FROM+'/*_uv.pre-cal_*.pre-cal.ms.archive '+LOCATION)
+# os.system('cp -r '+FROM+'/image_full_ampphase_di_m.NS.mask01.fits '+LOCATION)
+# os.system('cp -r '+FROM+'/image_full_ampphase_di_m.NS.DicoModel '+LOCATION)
+# os.system('cp -r '+FROM+'/*_uv.pre-cal_*.pre-cal.ms.archive '+LOCATION)
 print('Finished moving files')
 
 #FLAG TIME
@@ -40,7 +40,7 @@ if args.time_flag:
         os.system('{SINGULARITY} python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf {time} -ms {MS}'.format(SINGULARITY=SINGULARITY, MS=MS, time=' '.join(args.time_flag)))
 
 #MAKE LIST WITH MEASUREMENT SETS
-os.system('ls -1d {LOCATION}/*.goodtimes > {LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION))
+# os.system('ls -1d {LOCATION}/*.goodtimes > {LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION))
 
 with open('/home/jurjendejong/scripts/lofar_helpers/DDF_scripts/ddf.txt') as f:
     lines = [l.replace('\n','') for l in f.readlines()]
