a1784301ea04145569f405414dd3b6854c9fac7f
jurjen93 <jurjendejong93@gmail.com>
2021-06-15 13:43:43 +0200

    test

--- merge_optimal.py
+++ merge_optimal.py
@@ -16,13 +16,11 @@ def angular_distance(p1, p2):
     """angular distance for points in ra and dec"""
     return acos(sin(p1[1])*sin(p2[1])+cos(p1[1])*cos(p2[1])*cos(p1[0]-p2[0]))
 
-
 parser = ArgumentParser()
 parser.add_argument('-f', '--file', type=str, help='fitsfile name')
 parser.add_argument('-ac', '--angular_cutoff', type=float, default=None, help='angular distances higher than this value from the center will be excluded from the box selection')
+parser.add_argument('-in', '--inside', type=bool, default=False, help='keep directions inside the angular cutoff')
 args = parser.parse_args()
-
-
 
 hdu = fits.open(args.file)[0]
 header = WCS(hdu.header, naxis=2).to_header()
@@ -35,4 +33,5 @@ for dir in H.root.sol000.source[:]:
     position = [radian_to_degree(i) for i in dir[1]]
     if angular_distance(center, position)>args.angular_cutoff:
         print(dir)
+        print(position)
 H.close()
