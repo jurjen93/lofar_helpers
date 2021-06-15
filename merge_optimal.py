2b41b448a7392427f9c0d850d145441908bc5856
jurjen93 <jurjendejong93@gmail.com>
2021-06-15 16:38:53 +0200

    bugfix

--- merge_optimal.py
+++ merge_optimal.py
@@ -97,7 +97,7 @@ def create_new_soltab(h5_in_name, h5_out_name, directions, sources):
             current_sources = [source[0].decode('UTF-8') for source in solsetout.obj.source[:]]
             new_sources = [source for source in sources if source[0] not in current_sources]
             for n, ns in enumerate(new_sources):
-                new_sources[n]=(bytes('Dir'+str(n).zfill(2)), ns[1])
+                new_sources[n]=(bytes('Dir'+str(n).zfill(2), 'utf-8'), ns[1])
 
             if len(new_sources) > 0:
                 print(new_sources)
