21f680a80a2353ec03e73d622d25fd03949e31c7
jurjen93 <jurjendejong93@gmail.com>
2021-06-15 18:20:37 +0200

    bugfix

--- pipeline_scripts/strw/merge_complete_h5.sh
+++ pipeline_scripts/strw/merge_complete_h5.sh
@@ -2,7 +2,7 @@
 
 #merge all best solutions from LoTSS and own selfcal
 
-SOURCE=$1#L626678
+SOURCE=$1
 FOLDER=/net/tussenrijn/data2/jurjendejong/${SOURCE}
 SING_IMAGE=/net/rijn/data2/rvweeren/data/pill-latest.simg
 SING_BIND=/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2
