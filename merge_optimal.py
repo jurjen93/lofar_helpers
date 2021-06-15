ab4339c1636abea5b0f05a8a4be61d577bc51693
jurjen93 <jurjendejong93@gmail.com>
2021-06-15 16:16:34 +0200

    test

--- merge_optimal.py
+++ merge_optimal.py
@@ -27,7 +27,7 @@ def angular_distance(p1, p2):
     """angular distance for points in ra and dec"""
     return acos(sin(p1[1])*sin(p2[1])+cos(p1[1])*cos(p2[1])*cos(p1[0]-p2[0]))
 
-def create_new_soltab(h5_in, h5_out, solset, soltab, directions, sources):
+def create_new_soltab(h5_in, h5_out, directions, sources):
     """
     Create a new dataset in the h5 table
     :param filename: name of ourput file
@@ -36,69 +36,70 @@ def create_new_soltab(h5_in, h5_out, solset, soltab, directions, sources):
     :param dirs: directions to include
     """
 
-    print('Filter {solset}/{soltab} from {h5_in} into {h5_out}'.format(solset=solset, soltab=soltab, h5_in=h5_in, h5_out=h5_out))
 
-    h5_in = h5parm(h5_out, readonly=True)
+    h5_in = h5parm(h5_in, readonly=True)
     h5_out = h5parm(h5_out, readonly=False)
+    for ss in h5_in.getSolsetNames():
+        for st in h5_in.getSolset(ss).getSoltabNames():
+            print('Filter {solset}/{soltab} from {h5_in} into {h5_out}'.format(solset=ss, soltab=st, h5_in=h5_in, h5_out=h5_out))
 
-    solutiontable = h5_in.getSolset(solset).getSoltab(soltab)
-    axes = solutiontable.getValues()[1]
-    values_in = solutiontable.getValues()[0]
-    indexes = [list(axes['dir']).index(dir.decode('UTF-8')) for dir in directions]
-    axes['dir']=directions
-    dir_index = solutiontable.getAxesNames().index('dir')
+            solutiontable = h5_in.getSolset(ss).getSoltab(st)
+            axes = solutiontable.getValues()[1]
+            values_in = solutiontable.getValues()[0]
+            indexes = [list(axes['dir']).index(dir.decode('UTF-8')) for dir in directions]
+            axes['dir']=directions
+            dir_index = solutiontable.getAxesNames().index('dir')
+            shape = list(values_in.shape)
+            shape[dir_index]=len(directions)
+            if 'amplitude' in st:
+                values_new = zeros(shape)
+            elif 'phase' in st:
+                values_new = ones(shape)
+            else:
+                print('Skip {soltab}'.format(soltab=st))
+                return
+
+            for idx_new, idx_old in enumerate(indexes):
+                if dir_index == 0:
+                    if 'amplitude' in st:
+                        values_new[idx_new,...] *= values_in[idx_old, ...]
+                    elif 'phase' in st:
+                        values_new[idx_new,...] += values_in[idx_old, ...]
+                elif dir_index == 1:
+                    if 'amplitude' in st:
+                        values_new[:, idx_new,...] *= values_in[:, idx_old, ...]
+                    elif 'phase' in st:
+                        values_new[:, idx_new,...] += values_in[:, idx_old, ...]
+                elif dir_index == 2:
+                    if 'amplitude' in st:
+                        values_new[:, :, idx_new,...] *= values_in[:, :, idx_old, ...]
+                    elif 'phase' in st:
+                        values_new[:, :, idx_new,...] += values_in[:, :, idx_old, ...]
+                elif dir_index == 3:
+                    if 'amplitude' in st:
+                        values_new[:, :, :, idx_new,...] *= values_in[:, :, :, idx_old, ...]
+                    elif 'phase' in st:
+                        values_new[:, :, :, idx_new,...] += values_in[:, :, :, idx_old, ...]
+                elif dir_index == 4:
+                    if 'amplitude' in st:
+                        values_new[:, :, :, :, idx_new,...] *= values_in[:, :, :, :, idx_old,...]
+                    elif 'phase' in st:
+                        values_new[:, :, :, :, idx_new,...] += values_in[:, :, :, :, idx_old,...]
+
+            print('New number of sources {num}'.format(num=len(sources)))
+            print('Filtered output shape {shape}'.format(shape=values_new.shape))
+
+            solsetout = h5_out.getSolset(ss)
+            current_sources = [source[0].decode('UTF-8') for source in solsetout.obj.source[:]]
+            new_sources = [source for source in sources if source[0] not in current_sources]
+            if len(new_sources) > 0:
+                solsetout.obj.source.append(new_sources)
+
+
+            weights = ones(values_new.shape)
+            solsetout.makeSoltab(st, axesNames=list(axes.keys()), axesVals=list(axes.values()), vals=values_new,
+                                 weights=weights)
     h5_in.close()
-    shape = list(values_in.shape)
-    shape[dir_index]=len(directions)
-    if 'amplitude' in soltab:
-        values_new = zeros(shape)
-    elif 'phase' in soltab:
-        values_new = ones(shape)
-    else:
-        print('Skip {soltab}'.format(soltab=soltab))
-        return
-
-    for idx_new, idx_old in enumerate(indexes):
-        if dir_index == 0:
-            if 'amplitude' in soltab:
-                values_new[idx_new,...] *= values_in[idx_old, ...]
-            elif 'phase' in soltab:
-                values_new[idx_new,...] += values_in[idx_old, ...]
-        elif dir_index == 1:
-            if 'amplitude' in soltab:
-                values_new[:, idx_new,...] *= values_in[:, idx_old, ...]
-            elif 'phase' in soltab:
-                values_new[:, idx_new,...] += values_in[:, idx_old, ...]
-        elif dir_index == 2:
-            if 'amplitude' in soltab:
-                values_new[:, :, idx_new,...] *= values_in[:, :, idx_old, ...]
-            elif 'phase' in soltab:
-                values_new[:, :, idx_new,...] += values_in[:, :, idx_old, ...]
-        elif dir_index == 3:
-            if 'amplitude' in soltab:
-                values_new[:, :, :, idx_new,...] *= values_in[:, :, :, idx_old, ...]
-            elif 'phase' in soltab:
-                values_new[:, :, :, idx_new,...] += values_in[:, :, :, idx_old, ...]
-        elif dir_index == 4:
-            if 'amplitude' in soltab:
-                values_new[:, :, :, :, idx_new,...] *= values_in[:, :, :, :, idx_old,...]
-            elif 'phase' in soltab:
-                values_new[:, :, :, :, idx_new,...] += values_in[:, :, :, :, idx_old,...]
-
-    print('New number of sources {num}'.format(num=len(sources)))
-    print('Filtered output shape {shape}'.format(shape=values_new.shape))
-
-    solsetout = h5_out.getSolset(solset)
-    current_sources = [source[0].decode('UTF-8') for source in solsetout.obj.source[:]]
-    new_sources = [source for source in sources if source[0] not in current_sources]
-    if len(new_sources) > 0:
-        solsetout.obj.source.append(new_sources)
-
-
-    weights = ones(values_new.shape)
-    solsetout.makeSoltab(soltab, axesNames=list(axes.keys()), axesVals=list(axes.values()), vals=values_new,
-                         weights=weights)
-
     h5_out.close()
 
 parser = ArgumentParser()
@@ -134,9 +135,5 @@ for dir in H.root.sol000.source[:]:
         print('Remove {dir}'.format(dir=dir))
 H.close()
 
-h5 = h5parm(args.h5_file_out)
-for ss in h5.getSolsetNames():
-    for st in h5.getSolset(ss).getSoltabNames():
-        print(ss, st)
-        create_new_soltab(h5_in, h5_out, ss, st, directions, sources)
-h5.close()
+
+create_new_soltab(args.h5_file_in, args.h5_file_out, directions, sources)
