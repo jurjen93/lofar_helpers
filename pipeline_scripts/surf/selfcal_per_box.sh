61c770c147c40a5515a831ee1fa0fae1aeebda13
jurjen93 <jurjendejong93@gmail.com>
2021-07-16 17:21:53 +0200

    bugfix

--- pipeline_scripts/surf/selfcal_per_box.sh
+++ pipeline_scripts/surf/selfcal_per_box.sh
@@ -22,12 +22,12 @@ mkdir ${TO}/selfcal/box_${N}
 cp -r ${TO}/extract/*box_${N}.dysco.sub.shift.avg.weights.ms.archive0 ${TO}/selfcal/
 singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=${TO}/selfcal/Abell399-401_box_${N}.dysco.sub.shift.avg.weights.ms.archive0 msout.storagemanager=dysco msout=${TO}/selfcal/box_${N}/box_${N}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes msin.ntimes=1500 steps=[]
 cd ${TO}/selfcal/box_${N}
-singularity exec -B ${SING_BIND} ${SING_IMAGE} --noprofile --norc python ${SCRIPT_FOLDER}/runwscleanLBautoR.py -b ${TO}/boxes/box_${N}.reg --auto --imager=DDFACET box_${N}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes --helperscriptspath ${SCRIPT_FOLDER}/
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/runwscleanLBautoR.py -b ${TO}/boxes/box_${N}.reg --auto --imager=DDFACET box_${N}.dysco.sub.shift.avg.weights.ms.archive0.goodtimes --helperscriptspath ${SCRIPT_FOLDER}/
 echo "Finished selfcal for box_${N}"
 echo "-----FINISHED SELFCAL-----"
 
 #merge selfcals --> DONE IN SEPARATE SCRIPT NOW
 #SING_IMAGE=/home/lofarvwf-jdejong/singularities/pill-latest.simg
-#singularity exec -B ${SING_BIND} ${SING_IMAGE} --noprofile --norc python ${SCRIPT_FOLDER}/lofar_helpers/pipeline_scripts/surf/merge_selfcals.py -d ${TO}/selfcal
+#singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/lofar_helpers/pipeline_scripts/surf/merge_selfcals.py -d ${TO}/selfcal
 
 echo "----------END----------"
