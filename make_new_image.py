01d01756703bb2bdc2f07e95c2b9896e7733019e
jurjen93 <jurjendejong93@gmail.com>
2021-07-20 15:58:50 +0200

    test lotss

--- make_new_image.py
+++ make_new_image.py
@@ -15,19 +15,21 @@ parser = ArgumentParser()
 parser.add_argument('-from', '--from_where', type=str, help='directory where data is originally from', required=True)
 parser.add_argument('-to', '--to_where', type=str, help='destination directory', required=True)
 parser.add_argument('-tf', '--time_flag', nargs='+', help='flag time: start_time end_time', required=False)
+parser.add_argument('-h5', '--h5_file', default='complete_merged.h5')
 args = parser.parse_args()
 
 LOCATION=args.to_where#/net/tussenrijn/data2/jurjendejong/L626678
 FROM=args.from_where #/disks/paradata/shimwell/LoTSS-DR2/archive_other/L626678
+H5=args.h5_file
 SING_IMAGE='/net/rijn/data2/rvweeren/data/pill-latestMay2021.simg'
 SING_BIND='/net/tussenrijn'
 SINGULARITY=' '.join(['singularity exec -B', SING_BIND, SING_IMAGE])
 # SINGULARITY=' '.join(['singularity exec ', SING_IMAGE])
 
 #CREATE DESTINATION DIRECTORY IF NOT EXISTS
-# if not path.exists(LOCATION):
-#     os.system('mkdir {LOCATION}'.format(LOCATION=LOCATION))
-#     print('Created {LOCATION}'.format(LOCATION=LOCATION))
+if not path.exists(LOCATION):
+    os.system('mkdir {LOCATION}'.format(LOCATION=LOCATION))
+    print('Created {LOCATION}'.format(LOCATION=LOCATION))
 
 #CLEAN CACHE
 os.system('{SINGULARITY} CleanSHM.py'.format(SINGULARITY=SINGULARITY))
@@ -47,6 +49,12 @@ if args.time_flag:
         os.system('{SINGULARITY} python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf {time} -ms {MS}'.format(SINGULARITY=SINGULARITY, MS=MS, time=' '.join(args.time_flag)))
         print('Created ' + MS.split('/')[-1] + '.goodtimes')
 
+#MERGE LOTSS OUTER EDGE
+os.system('chmod u+x /home/jurjendejong/scripts/lofar_helpers/pipeline_scripts/strw/merge_complete_h5.sh')
+os.system('{SINGULARITY} python /home/jurjendejong/scripts/lofar_helpers/pipeline_scripts/strw/merge_complete_h5.sh L626678'.format(SINGULARITY=SINGULARITY))
+os.system('{SINGULARITY} mv {LOCATION_FROM}/{H5} {LOCATION}'.format(H5=H5, SINGULARITY=SINGULARITY, LOCATION=LOCATION, LOCATION_FROM="/".join(LOCATION.split("/")[0:-1])+"result_filtered"))
+os.system('{SINGULARITY} rm -rf {LOCATION_FROM}'.format(SINGULARITY=SINGULARITY, LOCATION_FROM="/".join(LOCATION.split("/")[0:-1])+"result_filtered"))
+
 #MAKE LIST WITH MEASUREMENT SETS
 os.system('ls -1d {LOCATION}/*.goodtimes > {LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION))
 
@@ -54,7 +62,7 @@ with open('/home/jurjendejong/scripts/lofar_helpers/DDF_scripts/ddf.txt') as f:
     lines = [l.replace('\n','') for l in f.readlines()]
     lines+=['--Data-MS={LOCATION}/big-mslist.txt'.format(LOCATION=LOCATION)]
     lines+=['--Predict-InitDicoModel={LOCATION}/image_full_ampphase_di_m.NS.DicoModel'.format(LOCATION=LOCATION)]
-    lines+=['--DDESolutions-DDSols={LOCATION}/complete_merged.h5:sol000/amplitude000+phase000'.format(LOCATION=LOCATION)]
+    lines+=['--DDESolutions-DDSols={LOCATION}/{H5}:sol000/amplitude000+phase000'.format(H5=H5, LOCATION=LOCATION)]
     lines+=['--Mask-External={LOCATION}/image_full_ampphase_di_m.NS.mask01.fits'.format(LOCATION=LOCATION)]
 
 #RUN DDF COMMAND
