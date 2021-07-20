ceb0d374cbfd97e32b969635de7be26123bbdae3
jurjen93 <jurjendejong93@gmail.com>
2021-07-20 15:58:50 +0200

    test lotss

--- supporting_scripts/flag_time.py
+++ supporting_scripts/flag_time.py
@@ -7,4 +7,3 @@ parser.add_argument('-ms', '--measurement_set', help='measurement set', required
 args = parser.parse_args()
 
 pt.taql('SELECT FROM {MS} WHERE TIME IN (SELECT DISTINCT TIME FROM {MS} OFFSET {time[0]} LIMIT {time[1]}) GIVING {MS}.goodtimes AS PLAIN'.format(MS=args.measurement_set, time=args.time_flag))
-# os.system(SINGULARITY+' DPPP msin={MS} msout.storagemanager=dysco msout={MS}.goodtimes msin.ntimes=1500 steps=[]'.format(LOCATION=LOCATION, MS=MS))
