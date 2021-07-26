477f967d1cc24259ef9c22f9c449484a0c26cb4b
jurjen93 <jurjendejong93@gmail.com>
2021-07-26 14:56:30 +0200

    cp fix

--- supporting_scripts/reinout/move_files_surf.sh
+++ supporting_scripts/reinout/move_files_surf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mv ~/scripts/lofar_helpers/supporting_scripts/reinout/*.py ~/scripts
-mv ~/scripts/lofar_helpers/h5_merger.py ~/scripts
+cp ~/scripts/lofar_helpers/supporting_scripts/reinout/*.py ~/scripts
+cp ~/scripts/lofar_helpers/h5_merger.py ~/scripts
