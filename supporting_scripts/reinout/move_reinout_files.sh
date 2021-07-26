00948640cfbba794e9173aa98e73cc795884fd3c
Jurjen Jong <jurjendejong@vdesk4.strw.leidenuniv.nl>
2021-07-26 14:08:00 +0200

    fix

--- supporting_scripts/reinout/move_reinout_files.sh
+++ supporting_scripts/reinout/move_reinout_files.sh
@@ -2,9 +2,9 @@
 
 FOLDER=/net/rijn/data2/rvweeren/LoTSS_ClusterCAL
 
-cp -r ${FOLDER}/lin2circ.py ~/scripts/reinout/ ~/scripts/lofar_helpers/supporting_scripts/reinout
-cp -r ${FOLDER}/plot_tecandphase.py ~/scripts/reinout/ ~/scripts/lofar_helpers/supporting_scripts/reinout
-cp -r ${FOLDER}/BLsmooth.py ~/scripts/reinout/ ~/scripts/lofar_helpers/supporting_scripts/reinout
-cp -r ${FOLDER}/lib_multiproc.py ~/scripts/reinout/ ~/scripts/lofar_helpers/supporting_scripts/reinout
-cp -r ${FOLDER}/runwscleanLBautoR.py ~/scripts/reinout/ ~/scripts/lofar_helpers/supporting_scripts/reinout
-cp -r ${FOLDER}/sub-sources-outside-cluster.py ~/scripts/reinout/ ~/scripts/lofar_helpers/supporting_scripts/reinout
+cp -r ${FOLDER}/lin2circ.py ~/scripts/lofar_helpers/supporting_scripts/reinout
+cp -r ${FOLDER}/plot_tecandphase.py ~/scripts/lofar_helpers/supporting_scripts/reinout
+cp -r ${FOLDER}/BLsmooth.py ~/scripts/lofar_helpers/supporting_scripts/reinout
+cp -r ${FOLDER}/lib_multiproc.py ~/scripts/lofar_helpers/supporting_scripts/reinout
+cp -r ${FOLDER}/runwscleanLBautoR.py ~/scripts/lofar_helpers/supporting_scripts/reinout
+cp -r ${FOLDER}/sub-sources-outside-cluster.py ~/scripts/lofar_helpers/supporting_scripts/reinout
