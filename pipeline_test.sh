e8d0c62062889423bab84a76d93f8f72f5e773a7
jurjen93 <jurjendejong93@gmail.com>
2021-07-27 16:40:01 +0200

    until

--- pipeline_test.sh
+++ pipeline_test.sh
@@ -41,13 +41,13 @@ echo "SELFCAL STARTED"
 for ((N=1;N<=${TOTAL_BOXES};N++))
 do
   echo "SELFCAL ${N}"
-  while [[ ! -f ${TO}/test/test_${N}.txt ]]
+  until [[ -f ${TO}/test/test_${N}.txt ]]
   do
     sleep 5
     echo "Still waiting for ${N}"
   done
   echo "selfcal_${N}" > ${TO}/test/test_selfcal_${N}.txt
-done &
+done
 wait
 
 echo "----------END----------"
