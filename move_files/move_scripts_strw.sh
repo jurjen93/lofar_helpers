2b97fe4b9078b652a238c94d91cfe286a10adfb6
jurjen93 <jurjendejong93@gmail.com>
2021-07-30 11:07:00 +0200

    wrong script

--- move_files/move_scripts_strw.sh
+++ move_files/move_scripts_strw.sh
@@ -7,4 +7,4 @@ cp -r ${FOLDER}/plot_tecandphase.py ~/scripts/lofar_helpers/supporting_scripts/r
 cp -r ${FOLDER}/BLsmooth.py ~/scripts/lofar_helpers/supporting_scripts/reinout
 cp -r ${FOLDER}/lib_multiproc.py ~/scripts/lofar_helpers/supporting_scripts/reinout
 cp -r ${FOLDER}/runwscleanLBautoR.py ~/scripts/lofar_helpers/supporting_scripts/reinout
-cp -r ${FOLDER}/sub-sources-outside-cluster.py ~/scripts/lofar_helpers/supporting_scripts/reinout
+cp -r ${FOLDER}/sub-sources-outside-region.py ~/scripts/lofar_helpers/supporting_scripts/reinout
