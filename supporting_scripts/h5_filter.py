eca2b9581de30fcdebabbb48211103159de5c516
jurjen93 <jurjendejong93@gmail.com>
2021-08-02 11:30:25 +0200

    test

--- supporting_scripts/h5_filter.py
+++ supporting_scripts/h5_filter.py
@@ -88,9 +88,9 @@ def create_new_soltab(h5_in_name, h5_out_name, directions, sources):
             else:
                 solsetout = h5_out.makeSolset(ss)
 
-            current_sources = [source[0].decode('UTF-8') for source in solsetout.obj.source[:]]
-            new_sources = [(bytes('Dir'+str(n).zfill(2), 'utf-8'), ns[1]) for n, ns in enumerate(sources)]
-            new_sources = [source for source in new_sources if source[0].decode('UTF-8') not in current_sources]
+            current_sources = [source[1] for source in solsetout.obj.source[:]]
+            new_sources = [source for source in sources if source[1] not in current_sources]
+            new_sources = [(bytes('Dir'+str(n).zfill(2), 'utf-8'), ns[1]) for n, ns in enumerate(new_sources)]
             axes['dir'] = [ns[0] for ns in new_sources]
 
             if len(new_sources) > 0:
