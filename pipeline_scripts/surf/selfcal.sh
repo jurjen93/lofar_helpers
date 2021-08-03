16b9b0855eff2417cc7f8bd995259927c962d4ba
jurjen93 <jurjendejong93@gmail.com>
2021-08-03 14:08:21 +0200

    test

--- pipeline_scripts/surf/selfcal.sh
+++ pipeline_scripts/surf/selfcal.sh
@@ -5,10 +5,10 @@
 #SBATCH --array=1-6
 
 SOURCE=$1 #L626678
-TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}/selfcal/box_${N}
+TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}/selfcal
 N=$2 #box number
 
-until [[ -f "${TO}/selfcal/box_${N}.${SLURM_ARRAY_TASK_ID}/command.txt" ]]
+until [[ -f "${TO}/box_${N}.${SLURM_ARRAY_TASK_ID}/command.txt" ]]
 do
   sleep 60
 done
