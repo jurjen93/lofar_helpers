a5f95e2341825d311c67620de166790e3fd0632e
jurjen93 <jurjendejong93@gmail.com>
2021-08-03 15:27:17 +0200

    test

--- pipeline_scripts/surf/selfcal.sh
+++ pipeline_scripts/surf/selfcal.sh
@@ -7,7 +7,7 @@
 SOURCE=$1 #L626678
 TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}/selfcal
 N=$2 #box number
-
+echo "${TO}/box_${N}.${SLURM_ARRAY_TASK_ID}"
 until [[ -d "${TO}/box_${N}.${SLURM_ARRAY_TASK_ID}" ]]
 do
   sleep 5
