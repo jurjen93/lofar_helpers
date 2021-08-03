013b90d054325d048577711732f4a2c678dfa42a
jurjen93 <jurjendejong93@gmail.com>
2021-08-03 15:04:01 +0200

    bugfix

--- pipeline_scripts/surf/write_selfcal_command.py
+++ pipeline_scripts/surf/write_selfcal_command.py
@@ -18,10 +18,6 @@ SING_BIND = "/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts"
 
 box_archives = sorted([b.split('/')[-1] for b in glob(TO + '/extract/*' + BOX + '.dysco.sub.shift.avg.weights.ms.archive*')])
 
-if len(box_archives)==0:
-    import sys
-    sys.exit("ERROR")
-
 while len(box_archives) != 6:
     time.sleep(5)
 
