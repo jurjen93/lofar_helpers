d3169f26ad73e97d37a395bbbb1dee230c8ffbc4
jurjen93 <jurjendejong93@gmail.com>
2021-08-03 15:28:58 +0200

    test

--- pipeline_scripts/surf/selfcal.sh
+++ pipeline_scripts/surf/selfcal.sh
@@ -7,11 +7,9 @@
 SOURCE=$1 #L626678
 TO=/project/lofarvwf/Share/jdejong/output/${SOURCE}/selfcal
 N=$2 #box number
-echo "${TO}/box_${N}.${SLURM_ARRAY_TASK_ID}"
-until [[ -d "${TO}/box_${N}.${SLURM_ARRAY_TASK_ID}" ]]
+
+until [[ -f "${TO}/box_${N}.${SLURM_ARRAY_TASK_ID}/command.sh" ]]
 do
   sleep 5
-  echo "NOT EXISTING"
 done
-sleep 5
 srun ${TO}/box_${N}.${SLURM_ARRAY_TASK_ID}/command.sh
