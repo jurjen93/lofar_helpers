8fa4ebf05e50ad0c292c6d37586c3e5727b24419
jurjen93 <jurjendejong93@gmail.com>
2021-08-05 20:10:24 +0200

    test

--- move_files/move_scripts_strw.sh
+++ move_files/move_scripts_strw.sh
@@ -8,3 +8,8 @@ cp -r ${FOLDER}/BLsmooth.py ~/scripts/lofar_helpers/supporting_scripts/reinout
 cp -r ${FOLDER}/lib_multiproc.py ~/scripts/lofar_helpers/supporting_scripts/reinout
 cp -r ${FOLDER}/runwscleanLBautoR.py ~/scripts/lofar_helpers/supporting_scripts/reinout
 cp -r ${FOLDER}/sub-sources-outside-region.py ~/scripts/lofar_helpers/supporting_scripts/reinout
+
+
+git add ~/scripts/lofar_helpers && \
+git commit -m "reinoutscripts" && \
+git push origin master
