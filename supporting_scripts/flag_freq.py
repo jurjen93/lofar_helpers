579ec56ed0dc56b199e18694d901c121386eea6b
jurjen93 <jurjendejong93@gmail.com>
2021-08-06 16:39:59 +0200

    bug

--- supporting_scripts/flag_freq.py
+++ supporting_scripts/flag_freq.py
@@ -9,7 +9,13 @@ import os
 
 parser = ArgumentParser()
 parser.add_argument('-ff', '--frequency_flag', nargs='+', help='freq flag: channels', required=True, type=str)
-parser.add_argument('-ms', '--measurement_set', help='measurement set', required=True)
+parser.add_argument('-msin', '--ms_in', help='measurement set', required=True)
+parser.add_argument('-msout', '--ms_out', help='measurement set', required=False, default='')
 args = parser.parse_args()
 
-os.system("DPPP msin={MSIN} msout={MSIN}.goodfreq msout.storagemanager=dysco steps=[pf] pf.type=preflagger pf.chan={FREQCHAN}".format(MSIN=args.measurement_set, FREQCHAN=args.frequency_flag))
+if not args.ms_out:
+    msout = args.ms_in+'.goodfreq'
+else:
+    msout = args.ms_out
+
+os.system("DPPP msin={MSIN} msout={MSOUT} msout.storagemanager=dysco steps=[pf] pf.type=preflagger pf.chan={FREQCHAN}".format(MSIN=args.ms_in, MSOUT=msout, FREQCHAN=args.frequency_flag))
