6a0dc4f05d0c2d1a4acb7223c1e9ef3c0a4dd207
jurjen93 <jurjendejong93@gmail.com>
2021-08-06 15:50:17 +0200

    test

--- pipeline_scripts/surf/write_selfcal_command.py
+++ pipeline_scripts/surf/write_selfcal_command.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from glob import glob
 import time
 import os
+import casacore.tables as ct
 
 parser = ArgumentParser()
 parser.add_argument('--box', type=str, help='Measurement set input')
@@ -21,22 +22,33 @@ box_archives = sorted([b.split('/')[-1] for b in glob(TO + '/extract/*' + BOX + 
 while len(box_archives) != 6:
     time.sleep(5)
 
-CUTTIME = False
-TIMECUT = str(0)
+#starting times fom measurement sets that have to be cutted for time
+CUTTIMES = [5019387068.011121, 5017577408.011121, 5020506668.011121]
+
+#starting times for measurement sets that have to be cutted for freq
+CUTFREQS = [5021107868.011121]
+
 
 if len(box_archives) == 6:
     for N, SUBBOX in enumerate(box_archives):
         N = str(N + 1)
-        if CUTTIME:
+        if ct.table(TO+"/extract/" + SUBBOX).getcol('TIME')[0] in CUTTIMES:
+            print('Cutting time for '+SUBBOX)
             cml = [
-                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " " + "DPPP msin=" + TO + "/extract/" + SUBBOX + " msout.storagemanager=dysco msout=" + TO + "/selfcal/" + BOX + '.' + N + "/" + SUBBOX + ".goodtimes msin.ntimes=" + TIMECUT + " steps=[]",
+                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " python "+args.script_path+"/supporting_scripts/flag_time.py -tf 0 1500 -msin "+TO+"/extract/" + SUBBOX+" -msout "+TO+"/selfcal/"+BOX + '.' + N+'/'+SUBBOX,
                 "cd "+TO+"/selfcal/" + BOX + '.' + N,
-                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " " + " python "+args.script_path+"/runwscleanLBautoR.py -b "+TO+"/boxes/" + BOX + ".reg --auto --imager=DDFACET --helperscriptspath="+args.script_path+" --autofrequencyaverage-calspeedup='True' "+SUBBOX+'.goodtimes']
+                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " python "+args.script_path+"/runwscleanLBautoR.py -b "+TO+"/boxes/" + BOX + ".reg --auto --imager=DDFACET --helperscriptspath="+args.script_path+" --autofrequencyaverage-calspeedup='True' "+SUBBOX+'.goodtimes']
+        elif ct.table(TO+"/extract/" + SUBBOX).getcol('TIME')[0] in CUTFREQS:
+            print('Cutting freq for '+SUBBOX)
+            cml = [
+                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " python "+args.script_path+"/supporting_scripts/flag_freq.py -ff='[15..19]' -msin "+TO+"/extract/" + SUBBOX+" -msout "+TO+"/selfcal/"+BOX + '.' + N+'/'+SUBBOX,
+                "cd "+TO+"/selfcal/" + BOX + '.' + N,
+                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " python "+args.script_path+"/runwscleanLBautoR.py -b "+TO+"/boxes/" + BOX + ".reg --auto --imager=DDFACET --helperscriptspath="+args.script_path+" --autofrequencyaverage-calspeedup='True' "+SUBBOX+'.goodtimes']
         else:
             cml = [
                 "cp -r "+TO+"/extract/" + SUBBOX + " " + TO+"/selfcal/" + BOX + '.' + N,
                 "cd "+TO+"/selfcal/" + BOX + '.' + N,
-                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " " + " python "+args.script_path+"/runwscleanLBautoR.py -b "+TO+"/boxes/" + BOX + ".reg --auto --imager=DDFACET --helperscriptspath="+args.script_path+" --autofrequencyaverage-calspeedup='True' "+SUBBOX]
+                "singularity exec -B " + SING_BIND + " " + SING_IMAGE + " python "+args.script_path+"/runwscleanLBautoR.py -b "+TO+"/boxes/" + BOX + ".reg --auto --imager=DDFACET --helperscriptspath="+args.script_path+" --autofrequencyaverage-calspeedup='True' "+SUBBOX]
         os.system("mkdir " + TO+"/selfcal/" + BOX + '.' + N)
         with open(TO+"/selfcal/" + BOX + '.' + N + "/command.sh", "w+") as f:
             f.write("#!/bin/bash\n")
