c15ff15c47a32458d1f91f25b774ff0adf7280b2
jurjen93 <jurjendejong93@gmail.com>
2021-08-07 13:10:06 +0200

    test

--- pipeline_scripts/surf/selfcal_A399.py
+++ pipeline_scripts/surf/selfcal_A399.py
@@ -25,13 +25,13 @@ CUTFREQS = [5021107868.011121]
 
 os.system("mkdir " + TO + "/selfcal/" + BOX)
 for MS in ms_archives:
-    if ct.table(TO + "/extract/" + MS).getcol('TIME')[0] in CUTTIMES:
+    if ct.table(TO + "/extract/" + BOX + '/'+ MS).getcol('TIME')[0] in CUTTIMES:
         print('Cutting time for '+MS)
         os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + TO + "/extract/" + MS + " -msout " + TO + "/selfcal/" + BOX + '/' + MS + '.goodtimes')
-    elif ct.table(TO + "/extract/" + MS).getcol('TIME')[0] in CUTFREQS:
+    elif ct.table(TO + "/extract/" + BOX + '/' + MS).getcol('TIME')[0] in CUTFREQS:
         print('Cutting freq for ' + MS)
-        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_freq.py -ff='[15..19]' -msin " + TO + "/extract/" + MS+" -msout " + TO + "/selfcal/" + BOX + '/' + MS + '.goodfreq')
+        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_freq.py -ff='[15..19]' -msin " + TO + "/extract/" + BOX + '/' + MS+" -msout " + TO + "/selfcal/" + BOX + '/' + MS + '.goodfreq')
     else:
-        os.system("cp -r " + TO + "/extract/" + MS + " " + TO + "/selfcal/" + BOX)
+        os.system("cp -r " + TO + "/extract/" + BOX + '/' + MS + " " + TO + "/selfcal/" + BOX)
 
 os.system("cd " + TO + "/selfcal/" + BOX + " && python /home/lofarvwf-jdejong/scripts/runwscleanLBautoR.py -b " + TO + "/boxes/" + BOX + ".reg --auto --imager=DDFACET --helperscriptspath=/home/lofarvwf-jdejong/scripts --autofrequencyaverage-calspeedup='True' " + BOX + '.dysco.sub.shift.avg.weights.ms.archive*')
---
+++ pipeline_scripts/surf/selfcal_parallal_array_A399.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -c 20
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
+#SBATCH --job-name=array
+#SBATCH --output=array_%A_%a.out
+#SBATCH --error=array_%A_%a.err
+#SBATCH --array=1-82%10
+
+SING_IMAGE=/home/lofarvwf-jdejong/singularities/lofar_sksp_fedora31_ddf.sif
+SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
+SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers
+TO=/project/lofarvwf/Share/jdejong/output/A399/selfcal
+
+START="$(date -u +%s)"
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/pipeline_scripts/surf/selfcal_A399.py --box ${SLURM_ARRAY_TASK_ID}
+END="$(date -u +%s)"
+echo "Selfcal in $((${END}-${START})) seconds" > ${TO}/finished/box_${N}.txt
