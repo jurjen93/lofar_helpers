b1d709d5835971e112a0e35c167bc49a7d4e7c24
jurjen93 <jurjendejong93@gmail.com>
2021-08-09 17:46:39 +0200

    test

--- reshuffle.py
+++ reshuffle.py
@@ -1,3 +1,3 @@
 from h5_merger import MergeH5
 
-MergeH5(h5_out='test.h5', h5_tables='test.h5', ms_files=[], convert_tec=True, merge_all_in_one=False).order_directions()
+MergeH5(h5_out='all_directions_p2.h5', h5_tables='all_directions_p2.h5', ms_files=[], convert_tec=True, merge_all_in_one=False).order_directions()
