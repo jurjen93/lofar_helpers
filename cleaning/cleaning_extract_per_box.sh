aa2ad4c44e37f806c1626715e103231636447b99
jurjen93 <jurjendejong93@gmail.com>
2021-08-12 10:35:26 +0200

    test

---
+++ cleaning/cleaning_extract_per_box.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BOX=$1
+
+FOLDER=/project/lofarvwf/Share/jdejong/output/A399/extract
+RESULT=${FOLDER}/box_${BOX}_result
+DATAFOLDER=${FOLDER}/box_${BOX}
+
+mkdir ${RESULT}
+mv ${DATAFOLDER}/*box_${BOX}.dysco.sub.shift.avg.weights.ms.archive0 ${RESULT}
+#mv ${RESULT} ${DATAFOLDER}
