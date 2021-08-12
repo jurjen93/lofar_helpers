8d646597bdad32d82afd65b6751360ba3b94cdb1
jurjen93 <jurjendejong93@gmail.com>
2021-08-12 09:42:27 +0200

    test

--- cleaning/cleaning_per_box.sh
+++ cleaning/cleaning_per_box.sh
@@ -8,5 +8,5 @@ DATAFOLDER=${FOLDER}/box_${BOX}
 
 mkdir ${RESULT}
 mv ${DATAFOLDER}/image_*.png ${RESULT}
-mv ${DATAFOLDER}/box_*.dysco.sub.shift.avg.weights.ms.archive*.avg ${RESULT}
+mv ${DATAFOLDER}/*box_*.dysco.sub.shift.avg.weights.ms.archive*.avg ${RESULT}
 mv ${DATAFOLDER}/plotlosoto* ${RESULT}
