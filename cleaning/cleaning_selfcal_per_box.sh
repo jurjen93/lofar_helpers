7f19fdf0a957815c6e2d7fe849a346216a0425ed
jurjen93 <jurjendejong93@gmail.com>
2021-08-12 10:56:01 +0200

    cleanup selfcal

--- cleaning/cleaning_selfcal_per_box.sh
+++ cleaning/cleaning_selfcal_per_box.sh
@@ -3,12 +3,15 @@
 BOX=$1
 
 FOLDER=/project/lofarvwf/Share/jdejong/output/A399/selfcal_test2
-RESULT=${FOLDER}/box_${BOX}_result
+#RESULT=${FOLDER}/box_${BOX}_result
 DATAFOLDER=${FOLDER}/box_${BOX}
 
-mkdir ${RESULT}
-mv ${DATAFOLDER}/image_*.png ${RESULT}
-mv ${DATAFOLDER}/*box_*.dysco.sub.shift.avg.weights.ms.archive*.avg ${RESULT}
-mv ${DATAFOLDER}/plotlosoto* ${RESULT}
-mv ${DATAFOLDER}/merged_selfcalcyle007*.h5 ${RESULT} # note that this is for max 8 cycles
+rm ${DATAFOLDER}/*.ddfcache
+rm ${DATAFOLDER}/*.py
+
+#mkdir ${RESULT}
+#mv ${DATAFOLDER}/image_*.png ${RESULT}
+#mv ${DATAFOLDER}/*box_*.dysco.sub.shift.avg.weights.ms.archive*.avg ${RESULT}
+#mv ${DATAFOLDER}/plotlosoto* ${RESULT}
+#mv ${DATAFOLDER}/merged_selfcalcyle007*.h5 ${RESULT} # note that this is for max 8 cycles
 #mv ${RESULT} ${DATAFOLDER}
