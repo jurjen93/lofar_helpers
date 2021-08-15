701109e73615c1e029cdff03df9f5d368c99b466
jurjen93 <jurjendejong93@gmail.com>
2021-08-15 10:31:28 +0200

    test

--- pipeline_scripts/surf/selfcal_parallel_array.sh
+++ pipeline_scripts/surf/selfcal_parallel_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -c 20
-#SBATCH -p infinite
+#SBATCH -c 24
+#SBATCH -p normal
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
 #SBATCH --job-name=array
