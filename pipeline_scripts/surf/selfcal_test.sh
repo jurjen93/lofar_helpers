34739e371103b83a7c08959dd72984f838ba5438
jurjen93 <jurjendejong93@gmail.com>
2021-08-15 13:11:16 +0200

    test

--- pipeline_scripts/surf/selfcal_test.sh
+++ pipeline_scripts/surf/selfcal_test.sh
@@ -19,6 +19,15 @@ echo "CPU ALLOC:"
 CPU_ALLOC=$(cat /proc/self/status | grep 'Cpus_allowed_list')
 echo $CPU_ALLOC
 
+# get CPU ID currently used by self
+echo "CPU ID:"
+CPU_ID=$(cat /proc/self/stat)
+echo $CPU_ID | gawk '{print $39}'
+# find all CPU ID's that are allocated (reserved) by self
+echo "CPU ALLOC:"
+CPU_ALLOC=$(cat /proc/self/status | grep 'Cpus_allowed_list')
+echo $CPU_ALLOC
+
 START="$(date -u +%s)"
 # get CPU ID currently used by self
 echo "CPU ID:"
