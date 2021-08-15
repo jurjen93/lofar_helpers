edc2eb65d9575048c845565cef6c6aff2b042ba2
jurjen93 <jurjendejong93@gmail.com>
2021-08-15 14:41:50 +0200

    test

--- pipeline_scripts/surf/extract_per_box.sh
+++ pipeline_scripts/surf/extract_per_box.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
-#SBATCH --constraint=skylake|rome
+#SBATCH --constraint="skylake|rome"
 
 FIELD=$1
 BOX=$2
