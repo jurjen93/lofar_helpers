a79d5a766829518c70962b1428aa55561c479433
jurjen93 <jurjendejong93@gmail.com>
2021-09-06 20:17:07 +0200

    merge

--- merge_selfcals.py
+++ merge_selfcals.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('-d', '--directory', type=str, help='directory path')
+parser.add_argument('-a', '--archive', type=str, help='archive number', default='0')
 parser.add_argument('-del', '--exclude_boxes', help='Exclude the following boxes (numbers only)')
 # parser.add_argument('-nd', '--make_new_direction', type=str2bool, nargs='?', const=True, default=True, help='make new directions')
 args = parser.parse_args()
@@ -26,17 +27,17 @@ h5_tables = []
 boxes_h5_list = glob('{directory}/box_*'.format(directory=args.directory))
 boxes_h5_list.sort(key=lambda x: get_digits(x))
 number_of_measurements = len(glob(boxes_h5_list[0]+'/'+'tecandphase0_selfcalcyle000*'))
-for i in range(number_of_measurements):
-    for box in boxes_h5_list:
-        try:
-            last_merged = sorted(glob('{box}/merged_selfcalcyle*_*.ms.archive{n}*h5'.format(box=box, n=str(i))))[-1]
-            print(last_merged)
-            if any(box in last_merged for box in excluded_boxes):
-                print('Exclude '+last_merged.split('/')[-1])
-            else:
-                h5_tables.append(last_merged)
-        except:
-            print("No merged_selfcal* in {box}".format(box=box.split('/')[-1]))
 
-    merge_h5(h5_out='all_directions{n}.h5'.format(n=str(i)),
-             h5_tables=h5_tables)
+for box in boxes_h5_list:
+    try:
+        last_merged = sorted(glob('{box}/merged_selfcalcyle*_*.ms.archive{n}*h5'.format(box=box, n=args.archive)))[-1]
+        print(last_merged)
+        if any(box in last_merged for box in excluded_boxes):
+            print('Exclude '+last_merged.split('/')[-1])
+        else:
+            h5_tables.append(last_merged)
+    except:
+        print("No merged_selfcal* in {box}".format(box=box.split('/')[-1]))
+
+merge_h5(h5_out='all_directions{n}.h5'.format(n=str(args.archive)),
+         h5_tables=h5_tables)
---
+++ merge_selfcals.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -c 4
+#SBATCH -p normal
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
+#SBATCH --job-name=merge
+#SBATCH --array=0-5
+
+SING_IMAGE=/home/lofarvwf-jdejong/singularities/lofar_sksp_fedora31_ddf_fixed.sif
+SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
+SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers
+
+
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/merge_selfcals.py -d $PWD -a ${SLURM_ARRAY_TASK_ID}
