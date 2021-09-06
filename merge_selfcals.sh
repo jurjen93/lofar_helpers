53dffb601d71d2bef5224397804aa1c19b45ff35
jurjen93 <jurjendejong93@gmail.com>
2021-09-06 20:21:15 +0200

    test

--- merge_selfcals.sh
+++ merge_selfcals.sh
@@ -6,9 +6,11 @@
 #SBATCH --job-name=merge
 #SBATCH --array=0-5
 
+SOURCE=$1
 SING_IMAGE=/home/lofarvwf-jdejong/singularities/lofar_sksp_fedora31_ddf_fixed.sif
 SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
 SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers
+SELFCAL=/project/lofarvwf/Share/jdejong/output/${SOURCE}/selfcal
 
 
-singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/merge_selfcals.py -d $PWD -a ${SLURM_ARRAY_TASK_ID}
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/merge_selfcals.py -d ${SELFCAL} -a ${SLURM_ARRAY_TASK_ID}
