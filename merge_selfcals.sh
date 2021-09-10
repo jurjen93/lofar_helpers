cd921dcf81d4c6c1e594ca426dae2f3db3159660
jurjen93 <jurjendejong93@gmail.com>
2021-09-10 18:19:02 +0200

    update

--- merge_selfcals.sh
+++ merge_selfcals.sh
@@ -12,4 +12,4 @@ SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
 SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers
 SELFCAL=/project/lofarvwf/Share/jdejong/output/${SOURCE}/selfcal
 
-cd ${SELFCAL} && singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/merge_selfcals.py -d ${SELFCAL} -a ${SLURM_ARRAY_TASK_ID}
+cd ${SELFCAL} && singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/merge_correct.py -d ${SELFCAL} -a ${SLURM_ARRAY_TASK_ID} -del 31
