ada90f9d00ed4bcd6e28fdfd6daacfce12969502
jurjen93 <jurjendejong93@gmail.com>
2021-10-03 16:58:28 +0200

    test

---
+++ move_files/move_box_images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for ((N=1;N<=82;N++))
+do
+  scp lofarvwf-jdejong@spider.surfsara.nl:/project/lofarvwf/Share/jdejong/output/A399/selfcal/box_${N}/image_007.app.restored.fits /net/tussenrijn/data2/jurjendejong/A399/box_images/box_${N}.fits
+done
+
