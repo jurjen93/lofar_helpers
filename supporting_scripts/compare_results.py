444fcccae18f63c8739d36c8b862bb78fad54fa6
jurjen93 <jurjendejong93@gmail.com>
2021-10-12 14:06:18 +0200

    test

--- supporting_scripts/compare_results.py
+++ supporting_scripts/compare_results.py
@@ -7,67 +7,25 @@ from argparse import ArgumentParser
 from glob import glob
 from astropy.nddata import Cutout2D
 
-def flatten(f):
-    """ Flatten a fits file so that it becomes a 2D image. Return new header and data """
 
-    naxis = f[0].header['NAXIS']
-    if naxis < 2:
-        sys.exit('Can\'t make map from this')
-    if naxis == 2:
-        return fits.PrimaryHDU(header=f[0].header, data=f[0].data)
+def make_cutout(fitsfile: str = None, region: str = None ):
 
-    w = WCS(f[0].header)
-    wn = WCS(naxis=2)
+    hdu = fits.open(fitsfile)
+    image_data = hdu[0].data
+    header = hdu.header
+    hdu.close()
 
-    wn.wcs.crpix[0] = w.wcs.crpix[0]
-    wn.wcs.crpix[1] = w.wcs.crpix[1]
-    wn.wcs.cdelt = w.wcs.cdelt[0:2]
-    wn.wcs.crval = w.wcs.crval[0:2]
-    wn.wcs.ctype[0] = w.wcs.ctype[0]
-    wn.wcs.ctype[1] = w.wcs.ctype[1]
+    r = pyregion.open(region)
+    ra  = r[0].coord_list[0]
+    dec = r[0].coord_list[1]
+    sizex = r[0].coord_list[2]
+    sizey = r[0].coord_list[3]
 
-    header = wn.to_header()
-    header["NAXIS"] = 2
-    copy = ('EQUINOX', 'EPOCH', 'BMAJ', 'BMIN', 'BPA', 'RESTFRQ', 'TELESCOP', 'OBSERVER')
-    for k in copy:
-        r = f[0].header.get(k)
-        if r is not None:
-            header[k] = r
-
-    slice = []
-    for i in range(naxis, 0, -1):
-        if i <= 2:
-            slice.append(np.s_[:], )
-        else:
-            slice.append(0)
-
-    hdu = fits.PrimaryHDU(header=header, data=f[0].data[tuple(slice)])
-    return hdu
-
-
-def mask_region(infilename,ds9region):
-
-    hdu=fits.open(infilename)
-    hduflat = flatten(hdu)
-
-    r = pyregion.open(ds9region)
-    manualmask = r.get_mask(hdu=hduflat)
-    # hdu[0].data[0][0][np.where(manualmask == True)] = 0.0
-    # hdu.writeto(outfilename,overwrite=True)
-
-    print(manualmask)
-
-def make_cutout(image_data, pos: tuple = None, size: tuple = (1000, 1000), hdu: str = None):
-    """
-    Make cutout from your image with this method.
-    pos (tuple) -> position in pixels
-    size (tuple) -> size of your image in pixel size, default=(1000,1000)
-    """
-    wcs = WCS(hdu.header, naxis=2)
+    wcs = WCS(header, naxis=2)
     out = Cutout2D(
         data=image_data,
-        position=pos,
-        size=size,
+        position=(ra, dec),
+        size=(sizex, sizey),
         wcs=wcs,
         mode='partial'
     )
@@ -93,5 +51,5 @@ if __name__ == '__main__':
     app_restored = args.app_restored
 
     for reg in region_files:
-        mask_region(image_final, region_files)
+        make_cutout(image_final, reg)
         break
