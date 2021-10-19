5900b15cf9f0f9b782a83e8d62bdb4dc1cb3b61f
jurjen93 <jurjendejong93@gmail.com>
2021-10-19 17:43:07 +0200

    test

--- make_new_image_A399.py
+++ make_new_image_A399.py
@@ -13,26 +13,27 @@ from glob import glob
 from os import path
 import casacore.tables as ct
 import tables
+import numpy as np
 
 TO='/net/tussenrijn/data2/jurjendejong/A399/result'
 FROM='/net/rijn/data2/jdejong/A399_DEEP'
 
 #CREATE DESTINATION DIRECTORY IF NOT EXISTS
-if not path.exists(TO):
-    os.system('mkdir {LOCATION}'.format(LOCATION=TO))
-    print('Created {LOCATION}'.format(LOCATION=TO))
+# if not path.exists(TO):
+#     os.system('mkdir {LOCATION}'.format(LOCATION=TO))
+#     print('Created {LOCATION}'.format(LOCATION=TO))
 
 #CLEAN CACHE
 os.system('CleanSHM.py')
 
 #MOVE FILES
 print('Moving files to '+TO)
-command = 'cp -r '+FROM+'/image_full_ampphase_di_m.NS.mask01.fits '+TO+ ' && '+\
-        'cp -r '+FROM+'/image_full_ampphase_di_m.NS.DicoModel '+TO+' && '+\
-        'scp lofarvwf-jdejong@spider.surfsara.nl:/project/lofarvwf/Share/jdejong/output/A399/selfcal/all_directions*.h5 '+TO+' && wait'
+# command = 'cp -r '+FROM+'/image_full_ampphase_di_m.NS.mask01.fits '+TO+ ' && '+\
+#         'cp -r '+FROM+'/image_full_ampphase_di_m.NS.DicoModel '+TO+' && '+\
+#         'scp lofarvwf-jdejong@spider.surfsara.nl:/project/lofarvwf/Share/jdejong/output/A399/selfcal/all_directions*.h5 '+TO+' && wait'
         # 'cp -r '+FROM+'/*_uv.pre-cal_*.pre-cal.ms.archive '+TO+' && wait'
 
-os.system(command)
+# os.system(command)
 print('Finished moving files')
 
 #----------------------------------------------------------------------------------------------------------------------
@@ -40,29 +41,29 @@ print('Finished moving files')
 #FLAG TIME AND FREQUENCY
 
 # starting times fom measurement sets that have to be cutted for time
-CUTTIMES = [5019387068.011121, 5019387064.005561, 5017577408.011121, 5017577404.005561, 5020506668.011121, 5020506664.005561]
-
-#starting times for measurement sets that have to be cutted for freq
-CUTFREQS = [5021107868.011121, 5021107864.005561]
-
-for MS in glob(FROM+'/*.ms.archive'):
-    t = ct.table(MS)
-    time = t.getcol('TIME')[0]
-    t.close()
-    if time in CUTTIMES:
-        print('Cutting time for '+MS)
-        os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 3000 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodtimes')
-    elif time in CUTFREQS:
-        print('Cutting freq for ' + MS)
-        if '127' not in MS:
-            os.system("cp -r " + MS + " " + TO)
-    else:
-        print('Copying for ' + MS)
-        os.system("cp -r " + MS + " " + TO)
-
-# important to wait until everything is ready before moving on
-while len(glob(FROM+'/*.ms.archive')) != len(glob(TO+'/*.pre-cal.ms.archive*'))+1:
-    print('TIME AND FREQUENCY FLAGGING')
+# CUTTIMES = [5019387068.011121, 5019387064.005561, 5017577408.011121, 5017577404.005561, 5020506668.011121, 5020506664.005561]
+#
+# #starting times for measurement sets that have to be cutted for freq
+# CUTFREQS = [5021107868.011121, 5021107864.005561]
+#
+# for MS in glob(FROM+'/*.ms.archive'):
+#     t = ct.table(MS)
+#     time = t.getcol('TIME')[0]
+#     t.close()
+#     if time in CUTTIMES:
+#         print('Cutting time for '+MS)
+#         os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 3000 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodtimes')
+#     elif time in CUTFREQS:
+#         print('Cutting freq for ' + MS)
+#         if '127' not in MS:
+#             os.system("cp -r " + MS + " " + TO)
+#     else:
+#         print('Copying for ' + MS)
+#         os.system("cp -r " + MS + " " + TO)
+#
+# # important to wait until everything is ready before moving on
+# while len(glob(FROM+'/*.ms.archive')) != len(glob(TO+'/*.pre-cal.ms.archive*'))+1:
+#     print('TIME AND FREQUENCY FLAGGING')
 #----------------------------------------------------------------------------------------------------------------------
 
 #MERGE LOTSS OUTER EDGE
@@ -88,7 +89,7 @@ for ms in DDS3_dict.items():
         new_h5.append(npz.split('/')[-1].replace('npz','h5 '))
 
     table = ct.table(ms[0])  # open table
-    t = table.getcol('TIME')[0]  # get first time element from measurement set
+    t = np.array(sorted(np.unique(table.getcol('TIME'))))[0]  # get first time element from measurement set
     table.close()  # close table
     diff = lambda ob_time: abs(ob_time - t)  # formula to compare difference between first time element of ms and observation times
     closest_value = min(list(soltable_times.keys()), key=diff)  # get closest value with lambda function
@@ -104,19 +105,19 @@ os.system('rm -rf /net/tussenrijn/data2/jurjendejong/A399/H5files')
 #----------------------------------------------------------------------------------------------------------------------
 
 #MAKE LIST WITH MEASUREMENT SETS
-os.system('ls -1d /net/tussenrijn/data2/jurjendejong/A399/result/*.pre-cal.ms.archive* > /net/tussenrijn/data2/jurjendejong/A399/result/big-mslist.txt'.format(LOCATION=TO))
-
-#----------------------------------------------------------------------------------------------------------------------
-
-#MAKE DDF COMMAND
-with open('/home/jurjendejong/scripts/lofar_helpers/DDF_scripts/ddf.txt') as f:
-    lines = [l.replace('\n','') for l in f.readlines()]
-    lines+=['--Data-MS=/net/tussenrijn/data2/jurjendejong/A399/result/big-mslist.txt']
-    lines+=['--Predict-InitDicoModel=/net/tussenrijn/data2/jurjendejong/A399/result/image_full_ampphase_di_m.NS.DicoModel']
-    lines+=['--DDESolutions-DDSols=/net/tussenrijn/data2/jurjendejong/A399/result/complete_merged*.h5:sol000/amplitude000+phase000']
-    lines+=['--Mask-External=/net/tussenrijn/data2/jurjendejong/A399/result/image_full_ampphase_di_m.NS.mask01.fits']
-
-#RUN DDF COMMAND
-print('Running DDF COMMAND')
-os.system(' '.join(['cd', TO, '&&'] + lines))
-print('Finished making new image')
+# os.system('ls -1d /net/tussenrijn/data2/jurjendejong/A399/result/*.pre-cal.ms.archive* > /net/tussenrijn/data2/jurjendejong/A399/result/big-mslist.txt'.format(LOCATION=TO))
+#
+# #----------------------------------------------------------------------------------------------------------------------
+#
+# #MAKE DDF COMMAND
+# with open('/home/jurjendejong/scripts/lofar_helpers/DDF_scripts/ddf.txt') as f:
+#     lines = [l.replace('\n','') for l in f.readlines()]
+#     lines+=['--Data-MS=/net/tussenrijn/data2/jurjendejong/A399/result/big-mslist.txt']
+#     lines+=['--Predict-InitDicoModel=/net/tussenrijn/data2/jurjendejong/A399/result/image_full_ampphase_di_m.NS.DicoModel']
+#     lines+=['--DDESolutions-DDSols=/net/tussenrijn/data2/jurjendejong/A399/result/complete_merged*.h5:sol000/amplitude000+phase000']
+#     lines+=['--Mask-External=/net/tussenrijn/data2/jurjendejong/A399/result/image_full_ampphase_di_m.NS.mask01.fits']
+#
+# #RUN DDF COMMAND
+# print('Running DDF COMMAND')
+# os.system(' '.join(['cd', TO, '&&'] + lines))
+# print('Finished making new image')
