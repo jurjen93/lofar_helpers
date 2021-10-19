c1bf80173a6ea23cccf369b843f633043943cf2f
jurjen93 <jurjendejong93@gmail.com>
2021-10-19 20:31:15 +0200

    L626678

--- make_new_image_A399.py
+++ make_new_image_A399.py
@@ -97,9 +97,9 @@ for ms in DDS3_dict.items():
     h5 = soltable_times[closest_value]
     print(h5)
     print(closest_value)
-    command.append('python /home/jurjendejong/scripts/lofar_helpers/h5_merger.py -out final_lotss_'+str(closest_value)+'.h5 -in '+' '.join(new_h5) + '--convert_tec 0')
-    command.append('python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/h5_filter.py -f /net/rijn/data2/jdejong/A399_DEEP/image_full_ampphase_di_m.NS.app.restored.fits -ac 2.5 -in false -h5out lotss_full_merged_filtered_'+str(closest_value)+'.h5 -h5in final_lotss_'+str(closest_value)+'.h5')
-    command.append('python /home/jurjendejong/scripts/lofar_helpers/h5_merger.py -out complete_merged_'+str(closest_value)+'.h5 -in lotss_full_merged_filtered_'+str(closest_value)+'.h5 ' + soltable_times[closest_value]+' --convert_tec 0')
+    command.append('python /home/jurjendejong/scripts/lofar_helpers/h5_merger.py -out final_lotss_' + str(closest_value) + '.h5 -in ' + ' '.join(new_h5) + '--convert_tec 0')
+    command.append('python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/h5_filter.py -f /net/rijn/data2/jdejong/A399_DEEP/image_full_ampphase_di_m.NS.app.restored.fits -ac 2.5 -in false -h5out lotss_full_merged_filtered_' + str(closest_value) + '.h5 -h5in final_lotss_' + str(closest_value) + '.h5')
+    command.append('python /home/jurjendejong/scripts/lofar_helpers/h5_merger.py -out complete_merged_' + str(closest_value)+'.h5 -in ' + soltable_times[closest_value] + ' lotss_full_merged_filtered_' + str(closest_value) + '.h5 --convert_tec 0')
 print('cd /net/tussenrijn/data2/jurjendejong/A399/H5files && '+' && '.join(command))
 os.system('cd /net/tussenrijn/data2/jurjendejong/A399/H5files && '+' && '.join(command))
 # os.system('mv /net/tussenrijn/data2/jurjendejong/A399/H5files/complete_merged*.h5 /net/tussenrijn/data2/jurjendejong/A399/result/ && wait')
