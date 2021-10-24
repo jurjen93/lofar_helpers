896dbbc0e5f0c075f87d10480fae9ab4a2318928
jurjen93 <jurjendejong93@gmail.com>
2021-10-24 21:35:42 +0200

    -r

--- pipeline_scripts/strw/selfcal_per_box_A399.sh
+++ pipeline_scripts/strw/selfcal_per_box_A399.sh
@@ -5,8 +5,8 @@ SING_IMAGE=/net/rijn/data2/rvweeren/data/pill-latest.simg
 SING_BIND=/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2
 SCRIPT_FOLDER=/home/jurjendejong/scripts/lofar_helpers
 
-cp /net/nieuwerijn/data2/jurjendejong/box_33_new_extract/Abell* /net/nieuwerijn/data2/jurjendejong/box_33_new_selfcal/
-cp /net/nieuwerijn/data2/jurjendejong/box_33_new_extract/box_33_new.reg /net/nieuwerijn/data2/jurjendejong/box_33_new_selfcal/
+cp -r /net/nieuwerijn/data2/jurjendejong/box_33_new_extract/Abell* /net/nieuwerijn/data2/jurjendejong/box_33_new_selfcal/
+cp -r /net/nieuwerijn/data2/jurjendejong/box_33_new_extract/box_33_new.reg /net/nieuwerijn/data2/jurjendejong/box_33_new_selfcal/
 cd /net/nieuwerijn/data2/jurjendejong/box_33_new_selfcal
 singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/pipeline_scripts/strw/prepare_data_A399.py --box ${BOX}
 cd selfcal
