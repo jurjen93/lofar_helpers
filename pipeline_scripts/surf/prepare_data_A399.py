f99ca711bbdeda625fd20e8434a4c13f22e89dfa
jurjen93 <jurjendejong93@gmail.com>
2021-11-02 14:10:27 +0100

    selfcalprep

--- pipeline_scripts/surf/prepare_data_A399.py
+++ pipeline_scripts/surf/prepare_data_A399.py
@@ -18,25 +18,24 @@ while len(ms_archives) != 6:
     time.sleep(5)
 
 #starting times fom measurement sets that have to be cutted for time
-CUTTIMES = [5019387068.011121, 5017577408.011121, 5020506668.011121]
+# CUTTIMES = [5019387068.011121, 5017577408.011121, 5020506668.011121]
 
 #starting times for measurement sets that have to be cutted for freq
 CUTFREQS = [5021107868.011121]
 
 os.system("mkdir -p " + TO + "/selfcal/" + BOX)
 for MS in ms_archives:
-    if ct.table(TO + "/extract/" + BOX + '/'+ MS).getcol('TIME')[0] in CUTTIMES:
-        print('Cutting time for '+MS)
-        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + TO + "/extract/" + BOX + '/' + MS + " -msout " + TO + "/selfcal/" + BOX + '/' + MS + '.goodtimes')
-    elif ct.table(TO + "/extract/" + BOX + '/' + MS).getcol('TIME')[0] in CUTFREQS:
+    if ct.table(TO + "/extract/" + BOX + '/' + MS).getcol('TIME')[0] in CUTFREQS:
         print('Cutting freq for ' + MS)
         os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_freq.py -ff='[15..19]' -msin " + TO + "/extract/" + BOX + '/' + MS+" -msout " + TO + "/selfcal/" + BOX + '/' + MS + '.goodfreq')
+        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + TO + "/extract/" + BOX + '/' + MS + '.goodfreq' + " -msout " + TO + "/selfcal/" + BOX + '/' + MS + '.goodfreq.goodtimes')
+        os.system("rm -rf " + TO + "/selfcal/" + BOX + '/' + MS + '.goodfreq')
     else:
-        os.system("cp -r " + TO + "/extract/" + BOX + '/' + MS + " " + TO + "/selfcal/" + BOX)
+        print('Cutting time for '+MS)
+        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + TO + "/extract/" + BOX + '/' + MS + " -msout " + TO + "/selfcal/" + BOX + '/' + MS + '.goodtimes')
 
-MS = [ms.split('/')[-1] for ms in glob(TO + '/selfcal/' + BOX + '/*' + BOX + '.dysco.sub.shift.avg.weights.ms.archive*')]
+
+MS = [ms.split('/')[-1] for ms in glob(TO + '/selfcal/' + BOX + '/*' + BOX + '.dysco.sub.shift.avg.weights.ms.archive*.goodtimes')]
 while len(MS) != 6:#important to wait until everything is ready before moving on to the next script --> selfcal
-    MS = [ms.split('/')[-1] for ms in glob(TO + '/selfcal/' + BOX + '/*' + BOX + '.dysco.sub.shift.avg.weights.ms.archive*')]
+    MS = [ms.split('/')[-1] for ms in glob(TO + '/selfcal/' + BOX + '/*' + BOX + '.dysco.sub.shift.avg.weights.ms.archive*.goodtimes')]
 print('DATA CUT AND PREPARED')
-
-# os.system("cd " + TO + "/selfcal/" + BOX + " && python /home/lofarvwf-jdejong/scripts/runwscleanLBautoR.py -b " + TO + "/boxes/" + BOX + ".reg --auto --imager=DDFACET --helperscriptspath=/home/lofarvwf-jdejong/scripts --autofrequencyaverage-calspeedup='True' " + ' '.join(MS))
