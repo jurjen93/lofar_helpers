a6fa2dd9ba45229f80e3fd55dec4b2204c54cb8d
jurjen93 <jurjendejong93@gmail.com>
2021-11-02 09:49:47 +0100

    test

--- cleaning/cleaning_selfcal_per_box.sh
+++ cleaning/cleaning_selfcal_per_box.sh
@@ -6,7 +6,7 @@ FOLDER=/project/lofarvwf/Share/jdejong/output/A399/selfcal
 #RESULT=${FOLDER}/box_${BOX}_result
 DATAFOLDER=${FOLDER}/box_${BOX}
 
-rm ${DATAFOLDER}/*.ddfcache
+rm -rf ${DATAFOLDER}/*.ddfcache
 rm ${DATAFOLDER}/*.py
 rm ${DATAFOLDER}/merged_selfcalcyle000*.h5
 rm ${DATAFOLDER}/merged_selfcalcyle001*.h5
@@ -17,23 +17,23 @@ rm ${DATAFOLDER}/merged_selfcalcyle005*.h5
 rm ${DATAFOLDER}/merged_selfcalcyle006*.h5
 rm -rf ${DATAFOLDER}/*.ddfcache
 rm -rf ${DATAFOLDER}/*box_${BOX}.dysco.sub.shift.avg.weights*
-rm -rf *templatejones.h5
-rm -rf final_merge*.h5
-rm -rf box_1.tar.gz
-rm antennaconstraint.p
-rm image_001*
-rm image_002*
-rm image_003*
-rm image_004*
-rm image_005*
-rm image_006*
-rm -rf scalarcomplexgain*
-rm -rf tecandphase*
-rm smoothness*
-rm solint.p
-rm soltypecycles.p
-rm nchan.p
-rm mslist.txt
+rm -rf ${DATAFOLDER}/*templatejones.h5
+rm -rf ${DATAFOLDER}/final_merge*.h5
+rm -rf ${DATAFOLDER}/box_1.tar.gz
+rm ${DATAFOLDER}/antennaconstraint.p
+rm ${DATAFOLDER}/image_001*
+rm ${DATAFOLDER}/image_002*
+rm ${DATAFOLDER}/image_003*
+rm ${DATAFOLDER}/image_004*
+rm ${DATAFOLDER}/image_005*
+rm ${DATAFOLDER}/image_006*
+rm -rf ${DATAFOLDER}/scalarcomplexgain*
+rm -rf ${DATAFOLDER}/tecandphase*
+rm ${DATAFOLDER}/smoothness*
+rm ${DATAFOLDER}/solint.p
+rm ${DATAFOLDER}/soltypecycles.p
+rm ${DATAFOLDER}/nchan.p
+rm ${DATAFOLDER}/mslist.txt
 
 #mkdir ${RESULT}
 #mv ${DATAFOLDER}/image_*.png ${RESULT}
