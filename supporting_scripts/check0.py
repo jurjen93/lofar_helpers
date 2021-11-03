c3b3cc90831fcd5743934825d28d47cc5e202cb7
jurjen93 <jurjendejong93@gmail.com>
2021-11-03 16:00:05 +0100

    test

--- supporting_scripts/check0.py
+++ supporting_scripts/check0.py
@@ -1,16 +1,17 @@
 import tables
 
 def has0coordinates(h5):
-    try:
-        h5 = tables.open_file(h5)
-        for c in h5.root.sol000.source[:]:
-            x, y = c[0][1][0], c[0][1][1]
-            if x==0. and y==0.:
-                h5.close()
-                return True
-        h5.close()
-    except:
-        pass
+    # try:
+    h5 = tables.open_file(h5)
+    for c in h5.root.sol000.source[:]:
+        x, y = c[0][1][0], c[0][1][1]
+        if x==0. and y==0.:
+            h5.close()
+            return True
+    h5.close()
+    # except:
+    #     h5.close()
+        # pass
     return False
 
 if __name__ == '__main__':
