02b55172cf2736705a947bf87bae5067689d038e
jurjen93 <jurjendejong93@gmail.com>
2021-11-03 16:02:11 +0100

    test

--- supporting_scripts/check0.py
+++ supporting_scripts/check0.py
@@ -4,7 +4,7 @@ def has0coordinates(h5):
     # try:
     h5 = tables.open_file(h5)
     for c in h5.root.sol000.source[:]:
-        x, y = c[0][1][0], c[0][1][1]
+        x, y = c[1]
         if x==0. and y==0.:
             h5.close()
             return True
