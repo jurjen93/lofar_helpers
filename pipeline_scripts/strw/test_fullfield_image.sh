4ae737224095049750dddc2cebaf15fb14b164e3
Jurjen Jong <jurjendejong@vdesk6.strw.leidenuniv.nl>
2021-11-25 19:40:13 +0100

    test

---
+++ pipeline_scripts/strw/test_fullfield_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#EXTRACT THE FULL FIELD ON 2.5 DEGREES
+
+SING_IMAGE=/net/lofar1/data1/sweijen/software/LOFAR/singularity/lofar_sksp_fedora31_ddf.sif
+SING_BIND=/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2
+SCRIPT_FOLDER=/home/jurjendejong/scripts/lofar_helpers
+
+singularity exec -B ${SING_BIND} /net/rijn/data2/rvweeren/data/pill-latest.simg python ${SCRIPT_FOLDER}/move_files/move_extract_files.py --frm /net/rijn/data2/jdejong/A399_DEEP --to /net/nieuwerijn/data2/jurjendejong/A399_extracted
+cd /net/nieuwerijn/data2/jurjendejong/A399_extracted
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python /net/rijn/data2/rvweeren/LoTSS_ClusterCAL/sub-sources-outside-region_noconcat.py --boxfile extracted.reg --freqavg=1 --timeavg=1 --overwriteoutput --noconcat --nophaseshift --adjustboxrotation=False --prefixname extr
+rm -rf /net/nieuwerijn/data2/jurjendejong/A399_extracted/L6*.ms
+rm -rf /net/nieuwerijn/data2/jurjendejong/A399_extracted/L6*.ms.archive
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/make_new_dicomodel.py
+
+SING_IMAGE=/net/rijn/data2/rvweeren/data/pill-latest.simg
+
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python /home/jurjendejong/scripts/lofar_helpers/make_new_image_A399_test.py
