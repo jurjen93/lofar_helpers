cf30e5bb27866f09f2d5c43e44d855d6e8be14df
jurjen93 <jurjendejong93@gmail.com>
2021-11-30 11:07:18 +0100

    bugfix

--- supporting_scripts/reinout/sub-sources-outside-region.py
+++ supporting_scripts/reinout/sub-sources-outside-region.py
@@ -1000,7 +1000,3 @@ for observation in range(number_of_unique_obsids(msfiles)):
             cmd += 'msin.nchan=' + str(nchanperblock) + ' ' + 'msout=' + msout + ' '
             print(cmd)
             run(cmd)
-
-    
-    
-    
