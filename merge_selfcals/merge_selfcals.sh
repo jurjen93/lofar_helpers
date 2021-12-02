618336a76d298b075ec38442a1845932dc279bf9
Jurjen Jong <jurjendejong@oolderplas.strw.leidenuniv.nl>
2021-12-02 10:44:05 +0100

    merge

--- merge_selfcals/merge_selfcals.sh
+++ merge_selfcals/merge_selfcals.sh
@@ -9,8 +9,8 @@
 SOURCE=$1
 SING_IMAGE=/home/lofarvwf-jdejong/singularities/pill-latest.simg
 SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
-SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers
+SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers/merge_selfcals
 SELFCAL=/project/lofarvwf/Share/jdejong/output/${SOURCE}/selfcal
 
 cd ${SELFCAL} && singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/merge_selfcals.py -d ${SELFCAL} -a ${SLURM_ARRAY_TASK_ID}
-singularity exec -B ${SING_BIND} ${SING_IMAGE} losoto ${SELFCAL}/all_direction${SLURM_ARRAY_TASK_ID}.h5 ~/scripts/lofar_helpers/supporting_scripts/parsets/plot${SLURM_ARRAY_TASK_ID}.parset
+singularity exec -B ${SING_BIND} ${SING_IMAGE} losoto ${SELFCAL}/all_directions${SLURM_ARRAY_TASK_ID}.h5 ~/scripts/lofar_helpers/supporting_scripts/parsets/plot${SLURM_ARRAY_TASK_ID}.parset
