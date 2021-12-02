cfa8f8ff315416f9f79fe24a8cde89a3be13493e
Jurjen Jong <jurjendejong@oolderplas.strw.leidenuniv.nl>
2021-12-02 10:39:02 +0100

    test

---
+++ merge_selfcals/merge_selfcals_include_A399.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -c 4
+#SBATCH -p normal
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
+#SBATCH --job-name=merge
+#SBATCH --array=0-5%1
+
+BOXES=$1
+SING_IMAGE=/home/lofarvwf-jdejong/singularities/pill-latest.simg
+SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
+SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers
+SELFCAL=/project/lofarvwf/Share/jdejong/output/A399/selfcal
+
+cd ${SELFCAL} && singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/merge_selfcals.py -d ${SELFCAL} -a ${SLURM_ARRAY_TASK_ID} --include_boxes ${BOXES}
+singularity exec -B ${SING_BIND} ${SING_IMAGE} losoto ${SELFCAL}/all_direction${SLURM_ARRAY_TASK_ID}.h5 ~/scripts/lofar_helpers/supporting_scripts/parsets/plot${SLURM_ARRAY_TASK_ID}.parset
