41ecb94a346a00c2cc6c48417c9b1460b199857c
jurjen93 <jurjendejong93@gmail.com>
2021-12-08 16:43:47 +0100

    test

--- pipeline_scripts/surf/selfcal_per_box_L626678_WSCLEAN.sh
+++ pipeline_scripts/surf/selfcal_per_box_L626678_WSCLEAN.sh
@@ -14,7 +14,7 @@ SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
 SCRIPT_FOLDER=/home/lofarvwf-jdejong/scripts/lofar_helpers
 TO=/project/lofarvwf/Share/jdejong/output/A399/selfcal
 
-echo "RUNNING NO BEAM CORRECTION WITH WSCLEAN"
+echo "RUNNING NO BEAM CORRECTION WITH WSCLEAN FOR L626678"
 
 # get CPU ID currently used by self
 echo "CPU ID:"
@@ -25,6 +25,6 @@ echo "CPU ALLOC:"
 CPU_ALLOC=$(cat /proc/self/status | grep 'Cpus_allowed_list')
 echo $CPU_ALLOC
 
-singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/pipeline_scripts/surf/prepare_data_A399.py --box ${BOX}
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python ${SCRIPT_FOLDER}/pipeline_scripts/surf/prepare_data_L626678.py --box ${BOX}
 cd /project/lofarvwf/Share/jdejong/output/A399/selfcal/box_${BOX}
 singularity exec -B ${SING_BIND} ${SING_IMAGE} python /home/lofarvwf-jdejong/scripts/runwscleanLBautoR.py -b /project/lofarvwf/Share/jdejong/output/A399/boxes/box_${BOX}.reg --auto --imager=WSCLEAN --helperscriptspath=/home/lofarvwf-jdejong/scripts --autofrequencyaverage-calspeedup --useaoflagger --uvmin=750 --tecfactorsolint=1.5 --no-beamcor --gainfactorsolint=2.0 *box_${BOX}.dysco.sub.shift.avg.weights.ms.archive*
