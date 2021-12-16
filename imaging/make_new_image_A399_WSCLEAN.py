44fe73605f49d5de26f1f5828f89de7058e28380
jurjen93 <jurjendejong93@gmail.com>
2021-12-16 13:38:37 +0100

    test

--- imaging/make_new_image_A399_WSCLEAN.py
+++ imaging/make_new_image_A399_WSCLEAN.py
@@ -34,11 +34,11 @@ for MS in glob(FROM+'/Abell399-401_extr*.ms.archive*'):
     if ct.table(MS).getcol('TIME')[0] in CUTFREQS:
         print('Cutting freq for ' + MS)
         os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_freq.py -ff='[15..19]' -msin " + MS+" -msout " + TO + '/' + MS.split('/')[-1] + '.goodfreq')
-        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 3000 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodfreq.goodtimes')
+        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodfreq.goodtimes')
         os.system("rm -rf " + TO + '/' + MS.split('/')[-1] + '.goodfreq')
     else:
         print('Cutting time for '+MS)
-        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 3000 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodtimes')
+        os.system("python /home/lofarvwf-jdejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodtimes')
 
 #----------------------------------------------------------------------------------------------------------------------
 
