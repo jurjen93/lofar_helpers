3718a46bd67e5b6dbfd635d88faf01b729772b1f
jurjen93 <jurjendejong93@gmail.com>
2021-12-17 11:30:22 +0100

    copy

--- supporting_scripts/make_goodtimes_goodfreq_A399.py
+++ supporting_scripts/make_goodtimes_goodfreq_A399.py
@@ -14,6 +14,7 @@ for MS in glob(FROM+'/Abell399-401_extr*.ms.archive*.avg'):
     if ct.table(MS).getcol('TIME')[0] in CUTFREQS:
         print('Cutting freq for ' + MS)
         os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_freq.py -ff='[15..19]' -msin " + MS+" -msout " + TO + '/' + MS.split('/')[-1] + '.goodfreq')
+        print('Cutting time for ' + MS)
         os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodfreq.goodtimes')
         os.system("rm -rf " + TO + '/' + MS.split('/')[-1] + '.goodfreq')
     else:
