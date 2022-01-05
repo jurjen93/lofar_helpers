dc848f299ba5fb5004443c49ed07814988bdaba2
jurjen93 <jurjendejong93@gmail.com>
2022-01-05 18:00:16 +0100

    test

--- imaging/make_new_image_WSCLEAN_test.py
+++ imaging/make_new_image_WSCLEAN_test.py
@@ -8,13 +8,26 @@ Example:
 __author__ = "Jurjen de Jong (jurjendejong@strw.leidenuniv.nl)"
 
 import os
+import sys
 from glob import glob
 from os import path
 import casacore.tables as ct
 import tables
 import numpy as np
+from argparse import ArgumentParser
 
-TO='/net/nieuwerijn/data2/jurjendejong/L626678_WSCLEAN_avg'
+parser = ArgumentParser()
+parser.add_argument('--N', type=str, help='archive number', required=True)
+args = parser.parse_args()
+
+N = args.N
+if N!='4':
+    MS = 'Abell399-401_extr.dysco.sub.shift.avg.weights.ms.archive' + N + '.avg.goodtimes'
+else:
+    MS = 'Abell399-401_extr.dysco.sub.shift.avg.weights.ms.archive' + N + '.avg.goodfreq.goodtimes'
+H5 = 'all_directions'+N+'.h5'
+
+TO='/net/nieuwerijn/data2/jurjendejong/Abell399-401_' + N
 FROM='/net/tussenrijn/data2/jurjendejong/A399_extracted_avg'
 
 #CREATE DESTINATION DIRECTORY IF NOT EXISTS
@@ -31,36 +44,18 @@ os.system('CleanSHM.py')
 # os.system('cd '+TO+' && python /net/rijn/data2/rvweeren/LoTSS_ClusterCAL/ds9facetgenerator.py --h5 all_directions0.h5 --DS9regionout '+
 #           TO+'/tess.reg --imsize 6000 '+'--ms '+FROM+'/Abell399-401_extr.dysco.sub.shift.avg.weights.ms.archive0.avg')
 
-# os.system('cp ' + FROM + '/tess.reg ' + TO)
-os.system('cp ' + FROM + '/all_directions0.h5 ' + TO + ' && wait')
-os.system('cp -r '+FROM+'/Abell399-401_extr.dysco.sub.shift.avg.weights.ms.archive0.avg.goodtimes ' + TO + ' && wait')
+os.system('cp ' + FROM + '/tess.reg ' + TO)
+os.system('cp ' + FROM + '/'+H5+' ' + TO + ' && wait')
+os.system('cp -r '+FROM+'/'+MS+' ' + TO + ' && wait')
+
+f = open(FROM+'/tess.reg')
+tess = f.read()
+f.close()
+H = tables.open_file(FROM+'/'+H5)
+if len(H.root.sol000.phase000.dir[:])!=len(tess.split('polygon'))-1:
+    sys.exit('ERROR: H5 and tess.reg do not match')
 
 CUTFREQS = [5021107868.011121, 5021107864.005561]
-
-# for MS in glob(FROM+'/L626678_concat.ms'):
-#     t = ct.table(MS)
-#     time = t.getcol('TIME')[0]
-#     t.close()
-#     if not (time in CUTFREQS and '127' in MS):
-#         print('Making goodtimes for'+MS)
-#         os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 3000 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodtimes')
-
-
-# for MS in glob(FROM+'/Abell399-401_extr*.ms.archive0.avg'):
-#     if ct.table(MS).getcol('TIME')[0] in CUTFREQS:
-#         print('Cutting freq for ' + MS)
-#         os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_freq.py -ff='[15..19]' -msin " + MS+" -msout " + TO + '/' + MS.split('/')[-1] + '.goodfreq')
-#         os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodfreq.goodtimes')
-#         os.system("rm -rf " + TO + '/' + MS.split('/')[-1] + '.goodfreq')
-#     else:
-#         print('Cutting time for '+MS)
-#         os.system("python /home/jurjendejong/scripts/lofar_helpers/supporting_scripts/flag_time.py -tf 0 1500 -msin " + MS + " -msout " + TO + '/' + MS.split('/')[-1] + '.goodtimes')
-
-
-#----------------------------------------------------------------------------------------------------------------------
-
-#MAKE LIST WITH MEASUREMENT SETS
-# os.system('ls -1d '+TO+'/L626678_concat.ms.goodtimes > '+TO+'/big-mslist.txt'.format(LOCATION=TO))
 
 #----------------------------------------------------------------------------------------------------------------------
 
@@ -69,11 +64,11 @@ CUTFREQS = [5021107868.011121, 5021107864.005561]
 with open('/'.join(__file__.split('/')[0:-1])+'/WSCLEAN_scripts/wsclean.txt') as f:
     lines = [l.replace('\n', '') for l in f.readlines()]
     lines += ['-facet-regions '+TO+'/tess.reg']
-    lines += ['-apply-facet-solutions '+TO+'/all_directions0.h5 amplitude000,phase000']
+    lines += ['-apply-facet-solutions '+TO+'/'+H5+' amplitude000,phase000']
     lines += ['-name image_test_L626678']
-    lines += [TO+'/Abell399-401_extr.dysco.sub.shift.avg.weights.ms.archive0.avg.goodtimes']
+    lines += [TO+'/'+MS]
 
-os.system('aoflagger '+TO+'/Abell399-401_extr.dysco.sub.shift.avg.weights.ms.archive0.avg.goodtimes && wait')
+os.system('aoflagger '+TO+'/'+MS+' && wait')
 
 cmd = ' '.join(['cd', TO, '&&'] + lines)
 #RUN DDF COMMAND
