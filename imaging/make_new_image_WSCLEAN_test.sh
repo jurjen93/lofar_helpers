ca5cdb09cd493083597592a7beb40472ddf4bb87
jurjen93 <jurjendejong93@gmail.com>
2022-01-06 11:33:34 +0100

    test

--- imaging/make_new_image_WSCLEAN_test.sh
+++ imaging/make_new_image_WSCLEAN_test.sh
@@ -14,5 +14,5 @@ singularity exec -B ${SING_BIND} ${SING_IMAGE} CleanSHM.py
 #mkdir -p ${TO}
 #cp ${FROM}/${H5} ${TO}
 #cp -r ${FROM}/${MS} ${TO}
-singularity exec -B ${SING_BIND} ${SING_IMAGE} python /net/rijn/data2/rvweeren/LoTSS_ClusterCAL/ds9facetgenerator.py --h5 ${H5} --DS9regionout ${TO}/tess.reg --imsize 6000 --ms ${TO}/${MS}
+singularity exec -B ${SING_BIND} ${SING_IMAGE} python /net/rijn/data2/rvweeren/LoTSS_ClusterCAL/ds9facetgenerator.py --h5 ${TO}/${H5} --DS9regionout ${TO}/tess.reg --imsize 6000 --ms ${TO}/${MS}
 singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} python ~/scripts/lofar_helpers/imaging/make_new_image_WSCLEAN_test.py --N ${N}
