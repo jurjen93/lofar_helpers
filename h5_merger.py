26af9d8dcebb2b4f44c8afc2088b9abc7ebb2060
jurjen93 <jurjendejong93@gmail.com>
2022-01-07 13:53:36 +0100

    test

--- h5_merger.py
+++ h5_merger.py
@@ -1327,7 +1327,7 @@ def test_h5_output(h5_out, tables_to_merge):
 
 def merge_h5(h5_out=None, h5_tables=None, ms_files=None, h5_time_freq=None, convert_tec=True, merge_all_in_one=False,
              lin2circ=False, circ2lin=False, add_directions=None, single_pol=None, no_pol=None, use_solset='sol000',
-             filtered_dir=None, add_CS=None):
+             filtered_dir=None, add_cs=None):
     """
     Main function that uses the class MergeH5 to merge h5 tables.
     :param h5_out (string): h5 table name out
@@ -1341,6 +1341,8 @@ def merge_h5(h5_out=None, h5_tables=None, ms_files=None, h5_time_freq=None, conv
     :param add_directions: add default directions by giving a list of directions (coordinates)
     :param single_pol: only one polarization
     :param use_solset: use specific solset number
+    :param filtered_dir: filter a specific list of directions from h5 file. Only lists allowed.
+    :param add_cs: use MS to replace super station with core station
     """
 
     h5_out = make_h5_name(h5_out)
@@ -1389,7 +1391,7 @@ def merge_h5(h5_out=None, h5_tables=None, ms_files=None, h5_time_freq=None, conv
     # pass
 
     #Add antennas
-    if add_CS:
+    if add_cs:
         if len(merge.ms) == 0:
             sys.exit('ERROR: --add_CS needs MS, given with --ms')
         merge.add_ms_antennas()
@@ -1478,7 +1480,7 @@ if __name__ == '__main__':
     parser.add_argument('--combine_h5', action='store_true', default=None, help='Combine h5 with different time axis into 1.')
     parser.add_argument('--usesolset', type=str, default='sol000', help='Choose a solset to merge from your input h5 files.')
     parser.add_argument('--filter_directions', type=str, default=None, help='Filter a specific list of directions from h5 file. Only lists allowed.')
-    parser.add_argument('--add_CS', action='store_true', default=None, help='Add core stations to antenna output')
+    parser.add_argument('--add_cs', action='store_true', default=None, help='Add core stations to antenna output')
     args = parser.parse_args()
 
     # check if solset name is accepted
@@ -1537,4 +1539,4 @@ if __name__ == '__main__':
              no_pol=args.no_pol,
              use_solset=args.usesolset,
              filtered_dir=filtered_dir,
-             add_CS=args.add_CS)
+             add_cs=args.add_cs)
