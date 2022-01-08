7b1a2eee5f01b2ef50a8481011861cc94c902665
jurjen93 <jurjendejong93@gmail.com>
2022-01-08 22:31:14 +0100

    test

--- imaging/make_new_image_WSCLEAN_test.sh
+++ imaging/make_new_image_WSCLEAN_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 N=$1
+NMITER=$2
 
 SING_BIND=/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2
 SING_IMAGE=/net/rijn/data2/rvweeren/data/pill-latestJune2021.simg
@@ -15,4 +16,4 @@ mkdir -p ${TO}
 cp ${FROM}/${H5} ${TO}
 cp -r ${FROM}/${MS} ${TO}
 singularity exec -B ${SING_BIND} ${SING_IMAGE} python /net/rijn/data2/rvweeren/LoTSS_ClusterCAL/ds9facetgenerator.py --h5 ${TO}/${H5} --DS9regionout ${TO}/tess.reg --imsize 6000 --ms ${TO}/${MS}
-singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} python ~/scripts/lofar_helpers/imaging/make_new_image_WSCLEAN_test.py --N ${N}
+singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} python ~/scripts/lofar_helpers/imaging/make_new_image_WSCLEAN_test.py --N ${N} --nmiter ${NMITER}
