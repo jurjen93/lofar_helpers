a624a677b60a363f66cf3feccd77baf61044746a
jurjen93 <jurjendejong93@gmail.com>
2022-01-12 18:53:53 +0100

    test

--- imaging/make_new_image_WSCLEAN_test_60.py
+++ imaging/make_new_image_WSCLEAN_test_60.py
@@ -21,12 +21,13 @@ else:
 
 MS = args.ms
 H5 = args.h5
+FACET = 'tess.reg'
 
 TO='/net/nieuwerijn/data2/jurjendejong/Abell399-401_' + N + '_60'
 FROM='/net/tussenrijn/data2/jurjendejong/A399_extracted_avg'
 
 
-f = open(TO+'/tess.reg')
+f = open(TO+'/'+FACET)
 tess = f.read()
 f.close()
 H = tables.open_file(TO+'/'+H5)
@@ -48,10 +49,10 @@ if len(H.root.sol000.phase000.dir[:])!=len(tess.split('polygon'))-1:
 #     lines += [TO+'/'+MS]
 
 lines = ['wsclean -size 1500 1500 -use-wgridder -no-update-model-required -reorder -weight briggs -0.5 -weighting-rank-filter 3 ' \
-        '-clean-border 1 -parallel-reordering  6 -padding 1.2 -auto-mask 2.5 -auto-threshold 0.5 -pol i -name image_test_A399_60 ' \
+        '-clean-border 1 -parallel-reordering 6 -padding 1.2 -auto-mask 2.5 -auto-threshold 0.5 -pol i -name image_test_A399_60 ' \
         '-scale 6arcsec -niter 50000 -mgain 0.8 -fit-beam -multiscale -channels-out 6 -join-channels -multiscale-max-scales 10 -nmiter ' + nmiter + \
-        ' -log-time -multiscale-scale-bias 0.7 -facet-regions '+TO+'/' \
-        'tess.reg ' \
+        ' -log-time -multiscale-scale-bias 0.7 -facet-regions '+TO+'/'+FACET+ \
+        ' -minuv-l 80.0' \
         '-parallel-gridding 6 -fit-spectral-pol 3 -taper-gaussian 60arcsec ' \
         '-apply-facet-solutions '+TO+'/'+H5+' amplitude000,phase000 '+ TO + \
         '/'+MS]
