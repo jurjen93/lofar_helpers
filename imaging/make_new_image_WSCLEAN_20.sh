a99cdb607724d60e47bf30c399b77abbe5b979d1
jurjen93 <jurjendejong93@gmail.com>
2022-01-14 17:21:40 +0100

    imaging

--- imaging/make_new_image_WSCLEAN_20.sh
+++ imaging/make_new_image_WSCLEAN_20.sh
@@ -8,7 +8,7 @@ MS=$2
 SING_BIND=/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2
 SING_IMAGE=/net/rijn/data2/rvweeren/data/pill-latestJune2021.simg
 SING_IMAGE_WSCLEAN=/net/lofar1/data1/sweijen/software/LOFAR/singularity/test/test_wsclean_facet_fix_sep30.sif
-TO=/net/nieuwerijn/data2/jurjendejong/Abell399-401
+TO=/net/nieuwerijn/data2/jurjendejong/Abell399-401_20
 FROM=/net/tussenrijn/data2/jurjendejong/A399_extracted_avg
 
 #check if directory exists
@@ -17,6 +17,9 @@ then
   echo "${TO} exists. Exit script"
   exit 0
 fi
+
+#cache
+singularity exec -B ${SING_BIND} ${SING_IMAGE} CleanSHM.py
 
 #make directory
 mkdir -p ${TO}
@@ -37,45 +40,45 @@ done
 cd ${TO}
 
 #make facet
-cp ${FROM}/tessupdate.reg ${TO} && wait
+cp ${FROM}/tess.reg ${TO} && wait
 #singularity exec -B ${SING_BIND} ${SING_IMAGE} python /net/rijn/data2/rvweeren/LoTSS_ClusterCAL/ds9facetgenerator.py \
 #--h5 ${TO}/${H5} \
 #--DS9regionout ${TO}/tess.reg \
 #--imsize 6000 \
-#--ms ${TO}/${MS}
+#--ms ${TO}/${MS_1}
 
 #run wsclean
-singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} wsclean \
--data-column DATA \
+singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} \
+wsclean \
+-size 3000 3000 \
 -use-wgridder \
--update-model-required \
+-no-update-model-required \
 -reorder \
--weight briggs \
--0.5 \
+-weight briggs -0.5 \
 -weighting-rank-filter 3 \
 -clean-border 1 \
--parallel-reordering 5 \
+-parallel-reordering 6 \
 -padding 1.2 \
 -auto-mask 2.5 \
 -auto-threshold 0.5 \
 -pol i \
--niter 150000 \
--mgain 0.7 \
+-name image_test_A399_20 \
+-scale 3arcsec \
+-niter 50000 \
+-mgain 0.8 \
 -fit-beam \
 -multiscale \
 -channels-out 6 \
--fit-spectral-pol 3 \
 -join-channels \
 -multiscale-max-scales 10 \
+-nmiter 11 \
 -log-time \
 -multiscale-scale-bias 0.7 \
--parallel-deconvolution 1600 \
--parallel-gridding 5 \
 -facet-regions ${TO}/tessupdate.reg \
+-parallel-gridding 6 \
+-fit-spectral-pol 3 \
+-taper-gaussian 20arcsec \
+-parallel-deconvolution 1600 \
 -apply-facet-solutions ${H5} amplitude000,phase000 \
--name image_test_A399 \
--size 6000 6000 \
--scale 1.5arcsec \
--nmiter 11 \
 -minuv-l 80.0 \
-${MS}
+${TO}/${MS}
