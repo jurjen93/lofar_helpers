3c4aa98b2781eda5aada2affaaa14158a35ef496
jurjen93 <jurjendejong93@gmail.com>
2022-01-18 15:52:41 +0100

    test

--- imaging/make_new_image_WSCLEAN.sh
+++ imaging/make_new_image_WSCLEAN.sh
@@ -8,14 +8,16 @@ MS=$2
 SING_BIND=/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2
 SING_IMAGE=/net/rijn/data2/rvweeren/data/pill-latestJune2021.simg
 SING_IMAGE_WSCLEAN=/net/lofar1/data1/sweijen/software/LOFAR/singularity/test/idgtest.sif
-TO=/net/nieuwerijn/data2/jurjendejong/Abell399-401
+TO=/net/${HOSTNAME%%.*}/data2/jurjendejong/Abell399-401
 FROM=/net/tussenrijn/data2/jurjendejong/A399_extracted_avg
+
+echo "Running on ${HOSTNAME%%.*}"
 
 #check if directory exists
 if [[ -f ${TO} ]]
 then
-  echo "${TO} exists. Exit script"
-  exit 0
+  echo "${TO} exists. Make Abell399-401_new"
+  TO=/net/${HOSTNAME%%.*}/data2/jurjendejong/Abell399-401_new
 fi
 
 #make directory
