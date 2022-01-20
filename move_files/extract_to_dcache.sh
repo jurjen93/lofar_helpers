3eff41fb2c1025fc21ded6f75d6dfc155990c1ad
jurjen93 <jurjendejong93@gmail.com>
2022-01-20 11:22:48 +0100

    dcache

--- move_files/extract_to_dcache.sh
+++ move_files/extract_to_dcache.sh
@@ -9,6 +9,6 @@ for BOXFOLDER in ${FOLDER}/box*
 do
   [[ ${BOXFOLDER} =~ ${REGEX} ]] # $pat must be unquoted
   BOX=${BASH_REMATCH[1]}
-  rclone --config=~/macaroon/maca_lofarvwf.conf mkdir maca_lofarvwf:/disk/${NAME}/extract/${BOX}
-  rclone --config=~/macaroon/maca_lofarvwf.conf copy ${FOLDER}/${BOX}/ maca_lofarvwf:/disk/${NAME}/extract/${BOX} -P
+  rclone --config=/home/lofarvwf-jdejong/macaroon/maca_lofarvwf.conf mkdir maca_lofarvwf:/disk/${NAME}/extract/${BOX}
+  rclone --config=/home/lofarvwf-jdejong/macaroon/maca_lofarvwf.conf copy ${FOLDER}/${BOX}/ maca_lofarvwf:/disk/${NAME}/extract/${BOX} -P
 done
