f1901a70dfadd6d49f1265f3fef65c70e1f68950
jurjen93 <jurjendejong93@gmail.com>
2022-02-01 10:40:26 +0100

    test

---
+++ imaging/surf/make_new_image_WSCLEAN_60_HAVG_oldsing.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH -c 24
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
+
+#input
+H5=$1
+MS=$2
+
+SING_BIND=/project/lofarvwf/Share/jdejong,/home/lofarvwf-jdejong/scripts
+SING_IMAGE=/home/lofarvwf-jdejong/singularities/pill-latest.simg
+SING_IMAGE_WSCLEAN=/home/lofarvwf-jdejong/singularities/idgtest_facetfix.sif
+
+TO=/project/lofarvwf/Share/jdejong/output/A399/imaging/Abell399-401_60_$(echo "$H5" | tr -cd ' ' | wc -c)_HAVG_oldsing
+FROM=/project/lofarvwf/Share/jdejong/output/A399/imaging/A399_extracted_avg
+TESS=tess60.reg
+
+#cache
+singularity exec -B ${SING_BIND} ${SING_IMAGE} CleanSHM.py
+
+#check if directory exists
+if [[ -f ${TO} ]]
+then
+  echo "${TO} exists. Exit script"
+  exit 0
+fi
+
+#make directory
+mkdir -p ${TO}
+
+#copy files
+for H in ${H5}
+do
+  cp ${FROM}/${H} ${TO}
+done
+
+#aoflagger
+for M in ${MS}
+do
+  cp -r ${FROM}/${M} ${TO} && wait
+  singularity exec -B ${SING_BIND} ${SING_IMAGE} aoflagger ${TO}/${M}
+done
+
+cd ${TO}
+
+#make facet
+cp ${FROM}/${TESS} ${TO} && wait
+
+#run wsclean
+singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} \
+wsclean \
+-data-column DATA \
+-size 1500 1500 \
+-use-wgridder \
+-no-update-model-required \
+-reorder \
+-weight briggs -0.5 \
+-weighting-rank-filter 3 \
+-clean-border 1 \
+-parallel-reordering 6 \
+-padding 1.2 \
+-auto-mask 2.5 \
+-auto-threshold 0.5 \
+-pol i \
+-name image_test_A399_60 \
+-scale 6arcsec \
+-niter 50000 \
+-mgain 0.8 \
+-fit-beam \
+-multiscale \
+-channels-out 6 \
+-join-channels \
+-multiscale-max-scales 10 \
+-nmiter 5 \
+-log-time \
+-multiscale-scale-bias 0.7 \
+-facet-regions ${TESS} \
+-parallel-gridding 6 \
+-fit-spectral-pol 3 \
+-taper-gaussian 60arcsec \
+-apply-facet-solutions ${H5// /,} amplitude000,phase000 \
+${MS}
