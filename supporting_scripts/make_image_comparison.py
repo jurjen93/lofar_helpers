ddde6db3607ada885343dc2bcc19908971de153f
jurjen93 <jurjendejong93@gmail.com>
2022-02-04 11:36:28 +0100

    tec error update

--- supporting_scripts/make_image_comparison.py
+++ supporting_scripts/make_image_comparison.py
@@ -73,25 +73,38 @@ class Imaging:
 
 vmin, vmax = 0.0005177041, 0.01294260291615501
 
-image_1 = Imaging('image_000.app.restored.fits', vmin=vmin, vmax=vmax)
-image_2 = Imaging('image_003.app.restored.fits', vmin=vmin, vmax=vmax)
-image_3 = Imaging('image_004.app.restored.fits', vmin=vmin, vmax=vmax)
-image_4 = Imaging('image_007.app.restored.fits', vmin=vmin, vmax=vmax)
+image_1 = Imaging('../image_000-MFS-image.fits', vmin=vmin, vmax=vmax)
+# image_2 = Imaging('image_003.app.restored.fits', vmin=vmin, vmax=vmax)
+# image_3 = Imaging('image_004.app.restored.fits', vmin=vmin, vmax=vmax)
+image_4 = Imaging('../image_007-MFS-image.fits', vmin=vmin, vmax=vmax)
 
 
-fig, axes = plt.subplots(figsize=(10, 10), nrows=1, ncols=3, subplot_kw={'projection': image_1.wcs})
+fig, axes = plt.subplots(figsize=(10, 10), nrows=1, ncols=2, subplot_kw={'projection': image_1.wcs}, sharey='all')
 axes[0].imshow(image_1.image_data, norm=SymLogNorm(linthresh=image_1.vmin / 10, vmin=image_1.vmin / 10, vmax=image_1.vmax / 2),
            origin='lower',
            cmap='CMRmap')
-axes[1].imshow(image_2.image_data, norm=SymLogNorm(linthresh=image_2.vmin / 10, vmin=image_2.vmin / 20, vmax=image_2.vmax),
+axes[0].set_xlabel('Galactic Longitude', size=15)
+axes[0].set_ylabel('Galactic Latitude', size=15)
+axes[0].tick_params(axis='both', which='major', labelsize=15)
+# axes[0].tick_params(axis='both', which='minor', labelsize=15)
+# axes[1].imshow(image_2.image_data, norm=SymLogNorm(linthresh=image_2.vmin / 10, vmin=image_2.vmin / 20, vmax=image_2.vmax),
+#            origin='lower',
+#            cmap='CMRmap')
+im = axes[1].imshow(image_4.image_data, norm=SymLogNorm(linthresh=image_1.vmin / 10, vmin=image_1.vmin / 20, vmax=image_1.vmax),
            origin='lower',
            cmap='CMRmap')
-im = axes[2].imshow(image_4.image_data, norm=SymLogNorm(linthresh=image_4.vmin / 10, vmin=image_4.vmin / 20, vmax=image_4.vmax),
-           origin='lower',
-           cmap='CMRmap')
+axes[1].set_xlabel('Galactic Longitude', size=15)
+# axes[1].set_ylabel('Galactic Latitude', size=15)
+axes[1].tick_params(axis='both', which='major', labelsize=15)
+axes[1].set_yticks([])
+axes[1].axes.yaxis.set_visible(False)
+axes[1].yaxis.set_visible(False)
+
+# axes[1].tick_params(axis='both', which='minor', labelsize=15)
 # divider = make_axes_locatable(axes[2])
 # cax2 = divider.append_axes("right", size="5%", pad=0.05)
-cbar = fig.colorbar(im, ax=axes, orientation='horizontal', shrink=0.6)
-cbar.set_label('Flux density [Jy/beam]')
-plt.subplots_adjust(left=0.1, bottom=0.3, right=0.9, top=0.5, wspace=0.5, hspace=0.1)
+cbar = fig.colorbar(im, ax=axes, orientation='horizontal', shrink=1)
+cbar.ax.tick_params(labelsize=15)
+cbar.set_label('Surface brightness [Jy/beam]', size=15)
+# plt.subplots_adjust(left=0.1, bottom=0.3, right=0.9, top=0.5, wspace=0.5, hspace=0.1)
 plt.show()
