6edfb38ef0977f6d52eaab469293d31957f0c341
jurjen93 <jurjendejong93@gmail.com>
2022-02-23 13:37:23 +0100

    imaging

--- imaging/make_new_image_WSCLEAN_20.sh
+++ imaging/make_new_image_WSCLEAN_20.sh
@@ -7,7 +7,7 @@ MS=$2
 #parameters
 SING_BIND=/tmp,/dev/shm,/disks/paradata,/data1,/net/lofar1,/net/rijn,/net/nederrijn/,/net/bovenrijn,/net/botlek,/net/para10,/net/lofar2,/net/lofar3,/net/lofar4,/net/lofar5,/net/lofar6,/net/lofar7,/disks/ftphome,/net/krommerijn,/net/voorrijn,/net/achterrijn,/net/tussenrijn,/net/ouderijn,/net/nieuwerijn,/net/lofar8,/net/lofar9,/net/rijn8,/net/rijn7,/net/rijn5,/net/rijn4,/net/rijn3,/net/rijn2
 SING_IMAGE=/net/rijn/data2/rvweeren/data/pill-latestJune2021.simg
-SING_IMAGE_WSCLEAN=/net/lofar1/data1/sweijen/software/LOFAR/singularity/test/idgtest_facetfix.sif
+SING_IMAGE_WSCLEAN=/net/lofar1/data1/sweijen/software/LOFAR/singularity/test/idgtest_23_02_2022.sif
 TO=/net/${HOSTNAME%%.*}/data2/jurjendejong/Abell399-401_20
 FROM=/net/rijn5/data2/jurjendejong/A399_extracted_avg
 TESS=tessupdate.reg
