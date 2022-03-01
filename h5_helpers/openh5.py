7a55c776f213da63b17b61ef386bd65019006a3e
jurjen93 <jurjendejong93@gmail.com>
2022-03-01 12:04:48 +0100

    reformat

--- h5_helpers/openh5.py
+++ h5_helpers/openh5.py
@@ -1,23 +1,26 @@
 import tables
 
+
 def list_opened():
     """List current open HDF5 files"""
     h5s = tables.file._open_files.filenames
-    print('Currently open --> '+', '.join(h5s))
+    print('Currently open --> ' + ', '.join(h5s))
     return h5s
 
+
 def force_close_all():
-    """Forcefully close all open HDF5 files"""
-    if len(tables.file._open_files._handlers)>0:
+    """Close all currently open HDF5 files by force"""
+    if len(tables.file._open_files._handlers) > 0:
         tables.file._open_files.close_all()
     else:
         print('No open HDF5 files')
 
+
 def force_close(h5):
-    """Forcefully close indivdual HDF5 file"""
+    """Close indivdual HDF5 file by force"""
     h5s = list(tables.file._open_files._handlers)
     for h in h5s:
-        if h.filename==h5:
+        if h.filename == h5:
             print('Closed by force --> ' + h5)
             h.close()
             return
