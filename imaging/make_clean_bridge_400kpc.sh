e5ff3b2940a0dda24e14978bdbc131466b6d149f
jurjen93 <jurjendejong93@gmail.com>
2022-03-15 18:08:38 +0100

    test

--- imaging/make_clean_bridge_400kpc.sh
+++ imaging/make_clean_bridge_400kpc.sh
@@ -15,74 +15,74 @@ TESS=tessupdate.reg
 NAME=final_image_A399
 
 #check if directory exists
-#if [[ -f ${TO} ]]
-#then
-#  echo "${TO} exists. Exit script"
-#  exit 0
-#fi
-#
+if [[ -f ${TO} ]]
+then
+  echo "${TO} exists. Exit script"
+  exit 0
+fi
+
 #cache
 singularity exec -B ${SING_BIND} ${SING_IMAGE} CleanSHM.py
-#
-##make directory
-#mkdir -p ${TO}
-#
-##copy files
-#for H in ${H5}
-#do
-#  cp ${FROM}/${H} ${TO}
-#done
-#
-##aoflagger
-#for M in ${MS}
-#do
-#  cp -r ${FROM}/${M} ${TO} && wait
-#  singularity exec -B ${SING_BIND} ${SING_IMAGE} aoflagger ${TO}/${M}
-#done
 
-#cd ${TO}
-echo ${TO}
+#make directory
+mkdir -p ${TO}
+
+#copy files
+for H in ${H5}
+do
+  cp ${FROM}/${H} ${TO}
+done
+
+#aoflagger
+for M in ${MS}
+do
+  cp -r ${FROM}/${M} ${TO} && wait
+  singularity exec -B ${SING_BIND} ${SING_IMAGE} aoflagger ${TO}/${M}
+done
+
+cd ${TO}
+
 #make facet
-#cp ${FROM}/${TESS} ${TO} && wait
-#
-## make first image
-#singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} wsclean \
-#-use-wgridder \
-#-no-update-model-required \
-#-reorder \
-#-weight briggs \
-#-0.5 \
-#-weighting-rank-filter 3 \
-#-clean-border 1 \
-#-parallel-reordering 5 \
-#-padding 1.2 \
-#-auto-mask 2.5 \
-#-auto-threshold 0.5 \
-#-pol i \
-#-niter 150000 \
-#-mgain 0.7 \
-#-fit-beam \
-#-multiscale \
-#-channels-out 6 \
-#-fit-spectral-pol 3 \
-#-join-channels \
-#-log-time \
-#-parallel-deconvolution 1600 \
-#-parallel-gridding 5 \
-#-facet-regions ${TESS} \
-#-apply-facet-solutions ${H5// /,} amplitude000,phase000 \
-#-name ${NAME}_compact \
-#-size 6000 6000 \
-#-scale 1.5arcsec \
-#-nmiter 7 \
-#-minuv-l 707.0 \
-#${MS} > logcompact.txt
+cp ${FROM}/${TESS} ${TO} && wait
+
+# make first image
+singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} wsclean \
+-use-wgridder \
+-no-update-model-required \
+-reorder \
+-weight briggs \
+-0.5 \
+-weighting-rank-filter 3 \
+-clean-border 1 \
+-parallel-reordering 5 \
+-padding 1.2 \
+-auto-mask 2.5 \
+-auto-threshold 0.5 \
+-pol i \
+-niter 150000 \
+-mgain 0.7 \
+-fit-beam \
+-multiscale \
+-channels-out 6 \
+-fit-spectral-pol 3 \
+-join-channels \
+-log-time \
+-parallel-deconvolution 1600 \
+-parallel-gridding 5 \
+-facet-regions ${TESS} \
+-apply-facet-solutions ${H5// /,} amplitude000,phase000 \
+-name ${NAME}_compact \
+-size 6000 6000 \
+-scale 1.5arcsec \
+-nmiter 7 \
+-minuv-l 707.0 \
+${MS} > logcompact.txt
 
 #mask compact objects
 singularity exec -B ${SING_BIND} ${SING_IMAGE_P2} \
 python /net/para10/data1/shimwell/software/killmsddf/new-install/DDFacet/SkyModel/MakeMask.py \
 --Th=3.0 \
---RestoredIm=_compact-MFS-image.fits
+--RestoredIm=${NAME}_compact-MFS-image.fits
 
 singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} wsclean \
 -use-wgridder \
@@ -94,7 +94,7 @@ singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} wsclean \
 -clean-border 1 \
 -parallel-reordering 5 \
 -padding 1.2 \
--fits-mask _compact-MFS-image.fits.mask.fits \
+-fits-mask ${NAME}_compact-MFS-image.fits.mask.fits \
 -pol i \
 -niter 150000 \
 -mgain 0.7 \
