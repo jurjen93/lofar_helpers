8b39e975aed095b8dade4b1ec9b8c8c24165fdbc
jurjen93 <jurjendejong93@gmail.com>
2022-09-16 09:37:13 +0200

    average script

---
+++ supporting_scripts/average_ms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -c 1
+
+SING_IMAGE=/home/lofarvwf-jdejong/singularities/pill-latest.simg
+SING_BIND=/project/lofarvwf/Share/jdejong
+
+MS_IN=$1
+
+singularity exec -B ${SING_BIND} ${SING_IMAGE} DP3 msin=${MS_IN} msout=avg_${MS_IN} steps=[av] msout.storagemanager=dysco steps=[av] av.type=averager av.freqstep=16 av.timestep=16
