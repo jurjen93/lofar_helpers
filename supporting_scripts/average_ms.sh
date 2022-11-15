5662484944fcf4436ea2812b1399465c9beb3460
jurjen93 <jurjendejong93@gmail.com>
2022-11-15 10:40:11 +0100

    test

--- supporting_scripts/average_ms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -c 4
-
-SING_IMAGE=/home/lofarvwf-jdejong/singularities/pill-latest.simg
-SING_BIND=/project/lofarvwf/Share/jdejong
-
-MS_IN=$1
-
-singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=${MS_IN} msout=avg_${MS_IN} steps=[av] msout.storagemanager=dysco steps=[av] av.type=averager av.freqstep=16 av.timestep=16
