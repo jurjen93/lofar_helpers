353e69e70c9847ce03f4696ffd0dea9857f73938
jurjen93 <jurjendejong93@gmail.com>
2023-04-24 11:24:03 +0200

    test

--- vlbi_helpers/scalarphasediff_selection/source_selection.sh
+++ vlbi_helpers/scalarphasediff_selection/source_selection.sh
@@ -5,12 +5,15 @@
 MSLIST=$1
 
 #SINGULARITY
-SING=$2 #/project/lofarvwf/Software/singularity/lofar_sksp_v4.0.2_znver2_znver2_noavx512_ddf_10_02_2023.sif
+BIND=$PWD
+SIMG=/net/achterrijn/data1/sweijen/software/containers/lofar_sksp_v4.0.2_x86-64_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
+#Spider --> /project/lofarvwf/Software/singularity/lofar_sksp_v4.0.2_znver2_znver2_noavx512_ddf_10_02_2023.sif
+#Leiden --> /net/achterrijn/data1/sweijen/software/containers/lofar_sksp_v4.0.2_x86-64_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
 
 mkdir -p h5output
 
 while read -r MS; do
-  singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG ./scalarphasediff.sh ${MS}
+  singularity exec -B $BIND $SIMG ./scalarphasediff.sh ${MS}
 done <$MSLIST
 
-singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG python scalarphasediff_output.py --h5 h5output/*.h5
+singularity exec -B $BIND $SIMG python scalarphasediff_output.py --h5 h5output/*.h5
