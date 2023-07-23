84ad7787951f60545a1f90b9caadd8539d623e3f
jurjen93 <jurjendejong93@gmail.com>
2023-07-23 22:29:18 +0200

    test

--- subtract_with_wsclean/subtract_with_wsclean.py
+++ subtract_with_wsclean/subtract_with_wsclean.py
@@ -254,8 +254,9 @@ class SubtractWSClean:
             # get number of rows
             nrows = ts.nrows()
             # make sure every slice has the same size
-            best_slice = get_largest_divider(nrows, 100)
+            best_slice = get_largest_divider(nrows, 10000)
             for c in range(0, nrows, best_slice):
+                print(c, best_slice)
                 if 'CORRECTED_DATA' in colnames:
                     print('SUBTRACT --> CORRECTED_DATA - MODEL_DATA')
                     if not self.onlyprint:
