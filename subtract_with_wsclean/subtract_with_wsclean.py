a9770e5a8f4e64295e1f5340a031427a0af17297
jurjen93 <jurjendejong93@gmail.com>
2023-07-25 00:00:08 +0200

    avg

--- subtract_with_wsclean/subtract_with_wsclean.py
+++ subtract_with_wsclean/subtract_with_wsclean.py
@@ -37,6 +37,57 @@ def get_largest_divider(inp, max=1000):
             return r
     sys.exit("ERROR: code should not arrive here.")
 
+def isfloat(num):
+    """
+    Check if value is a float
+    """
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+def parse_history(ms, hist_item):
+    """
+    Grep specific history item from MS
+
+    :param ms: measurement set
+    :param hist_item: history item
+
+    :return: parsed string
+    """
+    hist = os.popen('taql "SELECT * FROM '+ms+'::HISTORY" | grep '+hist_item).read().split(' ')
+    for item in hist:
+        if hist_item in item and len(hist_item)<=len(item):
+            return item
+    print('WARNING:' + hist_item + ' not found')
+    return None
+
+
+def get_time_preavg_factor(ms):
+    """
+    Get time pre-averaging factor (given by demixer.timestep)
+
+    :param ms: measurement set
+
+    :return: averaging integer
+    """
+    parse_str = "demixer.timestep="
+    parsed_history = parse_history(ms, parse_str)
+    avg_num = re.findall(r'\d+', parsed_history.replace(parse_str, ''))[0]
+    if avg_num.isdigit():
+        factor = int(float(avg_num))
+        if factor!=1:
+            print("WARNING: " + ms + " time has been pre-averaged with factor "+str(factor)+". This might cause time smearing effects.")
+        return factor
+    elif isfloat(avg_num):
+        factor = float(avg_num)
+        print("WARNING: parsed factor in " + ms + " is not a digit but a float")
+        return factor
+    else:
+        print("WARNING: parsed factor in " + ms + " is not a float or digit")
+        return None
+
 class SubtractWSClean:
     def __init__(self, mslist: list = None, region: str = None, localnorth: bool = True, onlyprint: bool = False):
         """
@@ -504,7 +555,7 @@ if __name__ == "__main__":
         print(polygon)
         phasecenter = polygon['dir'].values[0]
         freqavg = polygon['avg'].values[0]
-        timeavg = polygon['avg'].values[0]
+        timeavg = polygon['avg'].values[0]/get_time_preavg_factor(args.mslist[0])
         dirname = polygon['dir_name'].values[0]
     else:
         phasecenter = args.phasecenter
