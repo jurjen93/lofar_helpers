d40c1160931fe4122b6cbf075c13bebd676a96ab
jurjen93 <jurjendejong93@gmail.com>
2023-10-11 11:59:20 +0200

    modelupdate

--- subtract/subtract_with_wsclean.py
+++ subtract/subtract_with_wsclean.py
@@ -168,6 +168,10 @@ class SubtractWSClean:
             self.model_images = glob('*-????-model-pb.fits')
         elif len(glob('*-????-model.fits')) >=1:
             self.model_images = glob('*-????-model.fits')
+        elif len(glob('*-model-pb.fits')) >= 1:
+            self.model_images = glob('*-model-pb.fits')
+        elif len(glob('*-model.fits')) >= 1:
+            self.model_images = glob('*-model.fits')
         else:
             sys.exit("ERROR: No model images found.")
 
