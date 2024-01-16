f376514dbfab91765907ce4cfa233d8afba3748e
jurjen93 <jurjendejong93@gmail.com>
2024-01-16 12:04:13 +0100

    bug

--- source_detection/find_sources.py
+++ source_detection/find_sources.py
@@ -415,7 +415,8 @@ def main():
             cluster_indices = [idx for idx in cluster_indices if idx not in to_delete and idx not in to_ignore]
 
             if ((T[T['Source_id'] == n]['Peak_flux'][0] < rms * 2 or
-                T[T['Source_id'] == n]['Peak_flux_min'][0] < rms * (3/4)) and T['Total_flux_over_peak_flux'] < 2.5*beamarea)\
+                T[T['Source_id'] == n]['Peak_flux_min'][0] < rms * (3/4))
+                and T[T['Source_id'] == n]['Total_flux_over_peak_flux'][0] < 2.5*beamarea)\
                     or (T[T['Source_id'] == n]['Total_flux_over_peak_flux'][0] < beamarea/2
                         and T[T['Source_id'] == n]['Peak_flux'][0] < 2.5*rms):
                 make_cutout(fitsfile=fts, pos=tuple(c), size=(300, 300), savefits=f'deleted_sources/source_{m}_{n}.fits')
@@ -434,7 +435,7 @@ def main():
 
             elif ((T[T['Source_id'] == n]['Peak_flux_min'][0] < 1.5*rms or
                   T[T['Source_id'] == n]['Peak_flux'][0] < 3*rms)
-                  and T['Total_flux_over_peak_flux'] < 2*beamarea):
+                  and T[T['Source_id'] == n]['Total_flux_over_peak_flux'][0] < 2*beamarea):
 
                 make_cutout(fitsfile=fts, pos=tuple(c), size=(300, 300), savefits=f'weak_sources/source_{m}_{n}.fits')
                 make_image(f'weak_sources/source_{m}_{n}.fits', 'RdBu_r', 'components.reg')
