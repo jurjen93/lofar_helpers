58fd3c34ad2d81876c4d93353a640b21f976cfcb
jurjen93 <jurjendejong93@gmail.com>
2024-01-20 18:37:26 +0100

    t

--- source_detection/find_sources.py
+++ source_detection/find_sources.py
@@ -319,7 +319,7 @@ def make_image(fitsfiles, cmap: str = 'RdBu_r', components: str = None):
             if n!=0:
                 ax.set_title(fitsfile.split('/')[-2].replace('_', ' '))
 
-            cb = fig.colorbar(im, ax=ax, orientation='horizontal', shrink=0.8, pad=0.6)
+            cb = fig.colorbar(im, ax=ax, orientation='horizontal', shrink=0.6)
             cb.set_label('Surface brightness [mJy/beam]', size=14)
             cb.ax.tick_params(labelsize=14)
 
